d1c8237b8b84679ddffe79509dcf443aac2825bd
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2018-08-20 11:19:01 +0200

    use Component alias https://webpack.js.org/configuration/resolve/#resolve-alias
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- src/components/AppNavigation/AppNavigationItem.vue
+++ src/components/AppNavigation/AppNavigationItem.vue
@@ -95,7 +95,7 @@
 </template>
 
 <script>
-import PopoverMenu from '../PopoverMenu/PopoverMenu'
+import PopoverMenu from 'Components/PopoverMenu/PopoverMenu'
 import ClickOutside from 'vue-click-outside'
 import Vue from 'vue'
 
