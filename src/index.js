9abb0ad4da3f1e0d9342df94087d68612478ed3a
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2018-09-13 12:47:24 +0200

    CHange entry point, fix babel and update
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  */
-
+import '@babel/polyfill'
 import * as NcComponents from './components'
 
 function install(Vue) {
