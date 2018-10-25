c1b2e8bd09107989f183ad3a66c603c603214dc4
Julius Härtl <jus@bitgrid.net>
2018-10-25 15:34:26 +0200

    Better documentation and variable naming
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

---
+++ src/components/PopoverMenuButton/PopoverMenuButton.vue
@@ -0,0 +1,53 @@
+<template xmlns:>
+	<div class="popovermenu-wrapper">
+		<a class="button icon-more" @click="isOpen = !isOpen" />
+		<popover-menu :is-open="isOpen" :menu="menu" />
+	</div>
+</template>
+
+<script>
+import popoverMenu from '../PopoverMenu'
+
+export default {
+	name: 'PopoverMenuButton',
+	components: {
+		popoverMenu
+	},
+	props: {
+		menu: {
+			type: Array,
+			default: () => [
+				{
+					icon: 'icon-delete',
+					text: 'Delete item',
+					action: () => {
+						alert('Deleted!')
+					}
+				},
+				{
+					icon: 'icon-external',
+					text: 'Nextcloud website',
+					href: 'https://nextcloud.com'
+				},
+				{
+					icon: 'icon-details',
+					longtext: 'Add item',
+					href: 'https://nextcloud.com'
+				}
+			]
+		}
+	},
+	data: function() {
+		return {
+			isOpen: false
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+	.popovermenu-wrapper {
+		position: relative;
+		display: inline-block;
+	}
+</style>
