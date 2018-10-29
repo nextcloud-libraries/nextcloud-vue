64dcb392071312f9ddd8cde3f9b0097bec6d8700
dartcafe <github@dartcafe.de>
2018-10-29 06:10:37 +0100

    Update src/components/Avatar/Avatar.vue

--- src/components/Avatar/Avatar.vue
+++ src/components/Avatar/Avatar.vue
@@ -99,6 +99,13 @@ export default {
 		disableTooltip: {
 			type: Boolean,
 			default: false
+		},
+		/**
+		 * Define username is not a user name
+		 */
+		isNoUser: {
+			type: Boolean,
+			default: false
 		}
 	},
 	data() {
@@ -171,38 +178,14 @@ export default {
 			})
 		}
 	},
+	watch: {
+		user() {
+			this.userDoesNotExist = false
+			this.loadAvatarUrl()
+		}
+	},
 	mounted() {
-		/** Only run avatar image loading if either user or url property is defined */
-		if (!this.isUrlDefined && !this.isUserDefined) {
-			this.loadingState = false
-			this.userDoesNotExist = true
-			return
-		}
-
-		let avatarUrl = OC.generateUrl(
-			'/avatar/{user}/{size}',
-			{
-				user: this.user,
-				size: Math.ceil(this.size * window.devicePixelRatio)
-			})
-		// eslint-disable-next-line camelcase
-		if (this.user === OC.getCurrentUser().uid && typeof oc_userconfig !== 'undefined') {
-			avatarUrl += '?v=' + oc_userconfig.avatar.version
-		}
-		if (this.isUrlDefined) {
-			avatarUrl = this.url
-		}
-
-		let img = new Image()
-		img.onload = () => {
-			this.avatarUrlLoaded = avatarUrl
-			this.loadingState = false
-		}
-		img.onerror = () => {
-			this.userDoesNotExist = true
-			this.loadingState = false
-		}
-		img.src = avatarUrl
+		this.loadAvatarUrl()
 	},
 	methods: {
 		toggleMenu() {
@@ -223,6 +206,39 @@ export default {
 			}).catch(() => {
 				this.contactsMenuOpenState = false
 			})
+		},
+		loadAvatarUrl() {
+			/** Only run avatar image loading if either user or url property is defined */
+			if (!this.isUrlDefined && (!this.isUserDefined || this.isNoUser)) {
+				this.loadingState = false
+				this.userDoesNotExist = true
+				return
+			}
+
+			let avatarUrl = OC.generateUrl(
+				'/avatar/{user}/{size}',
+				{
+					user: this.user,
+					size: Math.ceil(this.size * window.devicePixelRatio)
+				})
+			// eslint-disable-next-line camelcase
+			if (this.user === OC.getCurrentUser().uid && typeof oc_userconfig !== 'undefined') {
+				avatarUrl += '?v=' + oc_userconfig.avatar.version
+			}
+			if (this.isUrlDefined) {
+				avatarUrl = this.url
+			}
+
+			let img = new Image()
+			img.onload = () => {
+				this.avatarUrlLoaded = avatarUrl
+				this.loadingState = false
+			}
+			img.onerror = () => {
+				this.userDoesNotExist = true
+				this.loadingState = false
+			}
+			img.src = avatarUrl
 		}
 	}
 }
