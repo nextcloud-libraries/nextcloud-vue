e7270d6a2cab3a3d0f4d4ffadfd27f1bfb98df6b
John Molakvoæ <skjnldsv@users.noreply.github.com>
2019-03-26 15:31:47 +0100

    Add isMobile mixin (#324)
    Add isMobile mixin
    
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -19,22 +19,9 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  */
-import * as NcComponents from './components'
 
-function install(Vue) {
-	Object.values(NcComponents).forEach((component) => {
-		Vue.component(component.name, component)
-	})
-}
+import isMobile from './isMobile'
 
-if (typeof window !== 'undefined' && window.Vue) {
-	install(window.Vue)
+export {
+	isMobile
 }
-
-export default {
-	install,
-	...NcComponents
-}
-export * from './components'
-export * from './directives'
-export * from './mixins'
