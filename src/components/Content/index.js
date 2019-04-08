bf0841039da7a590cfac80686ab677a28f850408
Christoph Wurst <christoph@winzerhof-wurst.at>
2019-04-08 10:49:53 +0200

    Rename AppContent -> Content
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -18,7 +18,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import AppContent from './AppContent'
+import Content from './Content'
 
-export default AppContent
-export { AppContent }
+export default Content
+export { Content }
