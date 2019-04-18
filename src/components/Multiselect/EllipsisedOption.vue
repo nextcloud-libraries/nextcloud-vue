0a0935c6bab9514e9a28904a20c5e5af68c8864d
John Molakvoæ <skjnldsv@users.noreply.github.com>
2019-04-18 13:03:26 +0200

    Enhancement/multiselect/ellipsis middle (#352)
    - Fix #261 
    - Moved to vue 2.6.x slot syntax
      https://vuejs.org/v2/guide/components-slots.html#Scoped-Slots 
      https://github.com/vuejs/rfcs/blob/master/active-rfcs/0001-new-slot-syntax.md
    
    ![Capture d’écran_2019-04-18_12-06-31](https://user-images.githubusercontent.com/14975046/56353811-8726bc80-61d2-11e9-82e6-26cf0c8c864e.png)
    
    @nextcloud/vue

---
+++ src/components/Multiselect/EllipsisedOption.vue
@@ -0,0 +1,86 @@
+<!--
+  - @copyright Copyright (c) 2018 John Molakvoæ <skjnldsv@protonmail.com>
+  -
+  - @author John Molakvoæ <skjnldsv@protonmail.com>
+  -
+  - @license GNU AGPL version 3 or any later version
+  -
+  - This program is free software: you can redistribute it and/or modify
+  - it under the terms of the GNU Affero General Public License as
+  - published by the Free Software Foundation, either version 3 of the
+  - License, or (at your option) any later version.
+  -
+  - This program is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  - GNU Affero General Public License for more details.
+  -
+  - You should have received a copy of the GNU Affero General Public License
+  - along with this program. If not, see <http://www.gnu.org/licenses/>.
+  -
+  -->
+
+<template>
+	<div class="name-parts" :title="name">
+		<span class="name-parts__first">{{ part1 }}</span>
+		<span v-if="part2" class="name-parts__last">{{ part2 }}</span>
+	</div>
+</template>
+<script>
+export default {
+	name: 'EllipsisedOption',
+	props: {
+		option: {
+			type: [String, Object],
+			required: true,
+			default: ''
+		},
+		label: {
+			type: String,
+			default: ''
+		}
+	},
+	computed: {
+		name() {
+			if (this.label) {
+				return this.option[this.label]
+			}
+			return this.option
+		},
+		needsTruncate() {
+			return this.name.length >= 10
+		},
+		part1() {
+			if (this.needsTruncate) {
+				// leave maximum 10 letters
+				var split = Math.min(Math.floor(this.name.length / 2), 10)
+				return this.name.substr(0, this.name.length - split)
+			}
+			return this.name
+		},
+		part2() {
+			if (this.needsTruncate) {
+				var split = Math.min(Math.floor(this.name.length / 2), 10)
+				return this.name.substr(this.name.length - split)
+			}
+			return ''
+		}
+
+	}
+}
+</script>
+<style lang="scss" scoped>
+.name-parts {
+	display: flex;
+	max-width: 100%;
+	&__first {
+		overflow: hidden;
+		white-space: nowrap;
+		text-overflow: ellipsis;
+	}
+	&__last {
+		// prevent whitespace from being trimmed
+		white-space: pre;
+	}
+}
+</style>
