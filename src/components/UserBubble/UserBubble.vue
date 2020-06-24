bd7bc35917e8a5150f1142565c47e3d169864444
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2020-06-24 15:10:32 +0200

    Do not render the full Popover on UserBubble if not necessary and add title slot support
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- src/components/UserBubble/UserBubble.vue
+++ src/components/UserBubble/UserBubble.vue
@@ -35,39 +35,65 @@ This component has the following slot:
 ### Examples
 
 ```vue
-<template>
-	<p>
-		Some text before
-		<user-bubble :user="'admin'" :display-name="'Admin Example'" :url="'/test'">
-			@admin@foreign-host.com
-		</user-bubble>
-		and after the bubble. <user-bubble :avatar-image="'icon-group'" :display-name="'test group xy'" :primary="true">Hey there!</user-bubble>
-	</p>
-</template>
+<p>
+	Some text before <user-bubble user="admin" display-name="Admin Example" :url="'/test'">@admin@foreign-host.com</user-bubble> and after the bubble.
+	<user-bubble avatar-image="icon-group" display-name="test group xyz" :primary="true">Hey there!</user-bubble>
+</p>
 ```
+
+### Example with title slot
+
+```vue
+<UserBubble
+	:margin="4"
+	:size="30"
+	display-name="Administrator"
+	user="admin">
+	<template #title>
+		<a href="#"
+			title="Remove user"
+			class="icon-close"
+			@click="alert" />
+	</template>
+</UserBubble>
+```
+
 </docs>
 <template>
-	<Popover trigger="hover focus"
+	<Popover :is="isPopoverComponent"
+		trigger="hover focus"
 		:open="open"
-		:disabled="popoverEmpty"
-		class="user-bubble-popover"
+		class="user-bubble__wrapper"
 		@update:open="onOpenChange">
+		<!-- Main userbubble structure -->
 		<div slot="trigger"
-			v-bind="linkOrNot"
-			class="user-bubble"
-			:class="primary ? 'user-bubble-primary' : ''">
-			<Avatar :url="!isUserAvatar && isIconUrl ? avatarImage : undefined"
-				:icon-class="!isUserAvatar && !isIconUrl ? avatarImage : undefined"
-				:user="isUserAvatar ? user : undefined"
-				:display-name="isUserAvatar ? displayName : undefined"
-				:size="16"
+			v-bind="isLinkComponent"
+			class="user-bubble__content"
+			:style="styles.content"
+			:class="primary ? 'user-bubble__content--primary' : ''">
+			<!-- Avatar -->
+			<Avatar :url="!isCustomAvatar && isAvatarUrl ? avatarImage : undefined"
+				:icon-class="!isCustomAvatar && !isAvatarUrl ? avatarImage : undefined"
+				:user="user"
+				:display-name="displayName"
+				:size="size - (margin * 2)"
+				:style="styles.avatar"
 				:disable-tooltip="true"
 				:disable-menu="true"
-				class="avatar" />
-			<h6 class="user">
-				{{ displayName ? displayName : user }}
+				class="user-bubble__avatar" />
+
+			<!-- Title -->
+			<h6 class="user-bubble__title">
+				{{ displayName || user }}
 			</h6>
+
+			<!-- @slot Optional slot just after the title -->
+			<span v-if="$slots.title" class="user-bubble__secondary">
+				<slot name="title" />
+			</span>
 		</div>
+
+		<!-- @slot Main Popover content on userbubble hover/focus -->
 		<slot />
 	</Popover>
 </template>
@@ -83,64 +109,130 @@ export default {
 		Avatar,
 	},
 	props: {
+		/**
+		 * Override generated avatar, can be an url or an icon class
+		 */
 		avatarImage: {
 			type: String,
-			default: '',
+			default: undefined,
 		},
+		/**
+		 * Provide the user id if this is a user
+		 */
 		user: {
 			type: String,
-			default: '',
+			default: undefined,
 		},
+		/**
+		 * Displayed label
+		 */
 		displayName: {
 			type: String,
 			required: true,
 		},
+		/**
+		 * Define the whole bubble as a link
+		 */
 		url: {
 			type: String,
-			default: '',
+			default: undefined,
+			validator: url => {
+				try {
+					url = new URL(url)
+					return !!url
+				} catch (error) {
+					return false
+				}
+			},
 		},
+		/**
+		 * Default popover state. Requires the UserBubble
+		 * to have some content to render inside the popover
+		 */
 		open: {
 			type: Boolean,
 			default: false,
 		},
+		/**
+		 * Use the primary colour
+		 */
 		primary: {
 			type: Boolean,
 			default: false,
 		},
+		/**
+		 * This is the height of the component
+		 */
+		size: {
+			type: Number,
+			default: 20,
+		},
+		/**
+		 * This is the margin of the avatar (size - margin = avatar size)
+		 */
+		margin: {
+			type: Number,
+			default: 2,
+		},
 	},
 	computed: {
-		isUserAvatar() {
-			if (!this.avatarImage) {
-				return true
-			}
-			return false
+		/**
+		 * If userbubble is empty, let's NOT
+		 * use the Popover component
+		 * @returns {string} 'Popover' or 'div'
+		 */
+		isPopoverComponent() {
+			return !this.popoverEmpty
+				? 'Popover'
+				: 'div'
 		},
-		isIconUrl() {
+
+		/**
+		 * Is the provided avatar url valid or not
+		 * @returns {boolean}
+		 */
+		isAvatarUrl() {
 			try {
-				// eslint-disable-next-line
-				new URL(this.avatarImage)
-				return true
+				const url = new URL(this.avatarImage)
+				return !!url
 			} catch (error) {
+				console.error(error)
 				return false
 			}
 		},
-		componentType() {
-			if (this.url) {
-				return 'div'
-			}
-			return 'a'
+		/**
+		 * Do we have a custom avatar or not
+		 * @returns {boolean}
+		 */
+		isCustomAvatar() {
+			return !!this.avatarImage
 		},
-		linkOrNot() {
-			if (this.url !== '') {
+
+		isLinkComponent() {
+			if (this.url && this.url.trim() !== '') {
 				return { is: 'a', href: this.url }
 			}
 			return { is: 'div' }
 		},
+
 		popoverEmpty() {
-			if (typeof this.$slots.default === 'undefined') {
-				return true
+			if ('default' in this.$slots) {
+				return false
 			}
-			return false
+			return true
+		},
+
+		styles() {
+			return {
+				content: {
+					height: this.size + 'px',
+					lineHeight: this.size + 'px',
+					borderRadius: this.size / 2 + 'px',
+				},
+				avatar: {
+					marginLeft: this.margin + 'px',
+				},
+			}
 		},
 	},
 	methods: {
@@ -152,33 +244,48 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.user-bubble-popover {
-	display: inline;
-	vertical-align: top;
-}
-
 .user-bubble {
-	display: flex;
-	height: 20px;
-	border-radius: 10px;
-	background-color: var(--color-background-dark);
+	&__wrapper {
+		// align inline with text
+		display: inline-block;
+		vertical-align: middle;
+		// shrink and allow grow to fit
+		min-width: 0;
+		max-width: 100%;
+	}
+
+	&__content {
+		display: inline-flex;
+		max-width: 100%;
+		background-color: var(--color-background-dark);
+
+		&--primary {
+			color: var(--color-primary-text);
+			background-color: var(--color-primary-element);
+		}
+
+		> :last-child {
+			// border radius left padding
+			padding-right: 8px;
+		}
+	}
+
+	&__avatar {
+		align-self: center;
+	}
+
+	&__title {
+		overflow: hidden;
+		white-space: nowrap;
+		text-overflow: ellipsis;
+	}
+
+	&__title,
+	&__secondary {
+		// proper spacing between avatar, title & slot
+		padding: 0;
+		padding-left: 4px;
+	}
 }
 
-.user-bubble-primary {
-	color: var(--color-primary-text);
-	background-color: var(--color-primary-element);
-}
-
-h6 {
-	line-height: 20px;
-}
-
-.avatar {
-	align-self: center;
-	margin-left: 2px;
-}
-
-.user {
-	margin: 0 8px 0 4px;
-}
 </style>
