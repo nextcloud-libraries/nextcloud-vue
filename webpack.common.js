8d46aeceeb55df57feeaefa25d857af9539ad049
Raimund Schlüßler <raimund.schluessler@mailbox.org>
2020-08-28 11:38:55 +0200

    Adjust sass-loader options
    Adjust for breaking changes in https://github.com/webpack-contrib/sass-loader/releases/tag/v9.0.0
    
    Signed-off-by: Raimund Schlüßler <raimund.schluessler@mailbox.org>

--- webpack.common.js
+++ webpack.common.js
@@ -93,7 +93,7 @@ module.exports = {
 					{
 						loader: 'sass-loader',
 						options: {
-							prependData: `$scope_version:${SCOPE_VERSION}; @import 'variables';`,
+							additionalData: `$scope_version:${SCOPE_VERSION}; @import 'variables';`,
 							/**
 							 * ! needed for resolve-url-loader
 							 */
