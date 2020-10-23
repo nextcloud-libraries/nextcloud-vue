50e46cdb61887d9205614db0629f54c33365f42b
Simon Belbeoch <simon.belbeoch@octo.com>
2020-10-24 01:09:47 +0200

    add missing user event tests for appSideBarTabs
     Signed-off-by: Simon Belbeoch <sbelbeoch@octo.com>
    
    Signed-off-by: Simon Belbeoch <simon.belbeoch@octo.com>

--- tests/unit/components/AppSidebar/AppSidebarTabs.spec.js
+++ tests/unit/components/AppSidebar/AppSidebarTabs.spec.js
@@ -92,18 +92,19 @@ describe('AppSidebarTabs.vue', () => {
 			expect(wrapper.find('nav').exists()).toBe(false)
 		})
 	})
-	describe('when only children of type AppSidebarTab are used', () => {
-		describe('when they are more than 1 child of type AppSidebarTab are used', () => {
+	describe('when only children of type AppSidebarTab is used', () => {
+		describe('when 3 children of type AppSidebarTab are used', () => {
 			beforeEach(() => {
 				wrapper = mount(AppSidebarTabs, {
 					slots: {
 						default: [
-							'<app-sidebar-tab id="1" icon="icon-details" name="Tab1">Tab1</app-sidebar-tab>',
-							'<app-sidebar-tab id="2" icon="icon-details" name="Tab2">Tab2</app-sidebar-tab>',
+							'<app-sidebar-tab id="first" icon="icon-details" name="Tab1">Tab1</app-sidebar-tab>',
+							'<app-sidebar-tab id="second" icon="icon-details" name="Tab2">Tab2</app-sidebar-tab>',
+							'<app-sidebar-tab id="last" icon="icon-details" name="Tab2">Tab2</app-sidebar-tab>',
 						],
 					},
 					stubs: {
-						// used to register custom components
+					// used to register custom components
 						AppSidebarTab,
 					},
 				})
@@ -114,15 +115,56 @@ describe('AppSidebarTabs.vue', () => {
 			it('display the nav element', () => {
 				expect(wrapper.find('nav').exists()).toBe(true)
 			})
-			it('display all the elements in li', () => {
+			it('display all the 3 elements in li', () => {
 				const liList = wrapper.findAll('nav>ul>li')
-				expect(liList.length).toEqual(2)
+				expect(liList.length).toEqual(3)
 			})
-			it('emit "update:active" event with the tab id when clicking on a tab', () => {
-				const firstLink = wrapper.find('nav>ul>li>a')
-				const id = firstLink.attributes('id')
-				firstLink.trigger('click')
-				expect(wrapper.emitted('update:active')[0]).toEqual([id])
+			it('emit "update:active" event with the selected tab id when clicking on a tab', () => {
+				const lastLink = wrapper.find('nav>ul>li:last-of-type>a')
+				lastLink.trigger('click')
+				expect(wrapper.emitted('update:active')[0]).toEqual(['last'])
+			})
+			it('emit "update:active" event with the first tab id when keydown pageup is pressed', () => {
+				const lastLink = wrapper.find('nav>ul>li:last-of-type>a')
+				lastLink.trigger('keydown.pageup')
+				expect(wrapper.emitted('update:active')[0]).toEqual(['first'])
+			})
+			it('emit "update:active" event with the last tab id when keydown pagedown is pressed', () => {
+				const lastLink = wrapper.find('nav>ul>li:last-of-type>a')
+				lastLink.trigger('keydown.pagedown')
+				expect(wrapper.emitted('update:active')[0]).toEqual(['last'])
+			})
+			describe('when we select the first element', () => {
+				beforeEach(() => {
+					wrapper.setData({ activeTab: 'first' })
+				})
+				it('does not emit "update:active" event when keydown left is pressed', () => {
+					expect(wrapper.emitted('update:active')).toBeFalsy()
+					const firstLink = wrapper.find('nav>ul>li>a')
+					firstLink.trigger('keydown.left')
+					expect(wrapper.emitted('update:active')).toBeFalsy()
+				})
+				it('emit "update:active" event with the next tab id when keydown right is pressed', () => {
+					const firstLink = wrapper.find('nav>ul>li>a')
+					firstLink.trigger('keydown.right')
+					expect(wrapper.emitted('update:active')[0]).toEqual(['second'])
+				})
+			})
+			describe('when we select the last element', () => {
+				beforeEach(() => {
+					wrapper.setData({ activeTab: 'last' })
+				})
+				it('emit "update:active" event with the previous tab id when keydown left is pressed', () => {
+					const lastLink = wrapper.find('nav>ul>li:last-of-type>a')
+					lastLink.trigger('keydown.left')
+					expect(wrapper.emitted('update:active')[0]).toEqual(['second'])
+				})
+				it('does not emit "update:active" event when keydown right is pressed', () => {
+					expect(wrapper.emitted('update:active')).toBeFalsy()
+					const lastLink = wrapper.find('nav>ul>li:last-of-type>a')
+					lastLink.trigger('keydown.right')
+					expect(wrapper.emitted('update:active')).toBeFalsy()
+				})
 			})
 		})
 		describe('when they is only 1 child of type AppSidebarTab are used', () => {
