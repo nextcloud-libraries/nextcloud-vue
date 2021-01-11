3ddde684818864b86c1e3453f9b2f09c1b715069
Christoph Wurst <christoph@winzerhof-wurst.at>
2021-01-11 12:15:24 +0100

    Allow ActionInput type=color
    Ref https://developer.mozilla.org/en-US/docs/Web/HTML/Element/Input/color
    
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/components/ActionInput/ActionInput.vue
+++ src/components/ActionInput/ActionInput.vue
@@ -124,7 +124,7 @@ export default {
 			validator(type) {
 				return ['date', 'datetime-local', 'month',
 					'number', 'password', 'search', 'tel',
-					'text', 'time', 'url', 'week'].indexOf(type) > -1
+					'text', 'time', 'url', 'week', 'color'].indexOf(type) > -1
 			},
 		},
 		/**
