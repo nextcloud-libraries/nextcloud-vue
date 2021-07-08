f4f9ca0880507769dfc2ffa75246d45bf839ac75
Christoph Wurst <christoph@winzerhof-wurst.at>
2021-07-08 16:41:37 +0200

    Add timezone select component
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

---
+++ src/components/TimezonePicker/TimezonePicker.vue
@@ -0,0 +1,122 @@
+<!--
+  - @copyright Copyright (c) 2021 Christoph Wurst <christoph@winzerhof-wurst.at>
+  -
+  - @author Christoph Wurst <christoph@winzerhof-wurst.at>
+  -
+  - @license GNU AGPL version 3 or any later version
+  -
+  - This program is free software: you can redistribute it and/or modify
+  - it under the terms of the GNU Affero General Public License as
+  - published by the Free Software Foundation, either version 3 of the
+  - License, or (at your option) any later version.
+  -
+  - This program is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  - GNU Affero General Public License for more details.
+  -
+  - You should have received a copy of the GNU Affero General Public License
+  - along with this program. If not, see <http://www.gnu.org/licenses/>.
+  -
+  -->
+
+<docs>
+### Example
+```vue
+<template>
+	<span>
+		<TimezonePicker v-model="tz" />
+	</span>
+</template>
+<script>
+export default {
+	data() {
+		return {
+			tz: 'Hawaiian Standard Time',
+		}
+	},
+}
+</script>
+```
+</docs>
+
+<template>
+	<Multiselect
+		:value="selectedTimezone"
+		:options="options"
+		:multiple="false"
+		:group-select="false"
+		:placeholder="placeholder"
+		group-values="regions"
+		group-label="continent"
+		track-by="timezoneId"
+		label="label"
+		open-direction="above"
+		@input="change" />
+</template>
+
+<script>
+import Multiselect from '../Multiselect'
+import {
+	getReadableTimezoneName,
+	getSortedTimezoneList,
+} from './timezone.js'
+import getTimezoneManager from './timezoneDataProviderService.js'
+import { t } from '../../l10n'
+
+export default {
+	name: 'TimezonePicker',
+	components: {
+		Multiselect,
+	},
+	props: {
+		/**
+		* An array of additional timezones to include with the standard database. Useful if there is a custom timezone, e.g. read from user data
+		*/
+		additionalTimezones: {
+			type: Array,
+			default: () => [],
+		},
+		/**
+		* The selected timezone. Use v-model for two-way binding. The default timezone is floating, which means a time independent of timezone. See https://icalendar.org/CalDAV-Access-RFC-4791/7-3-date-and-floating-time.html for details.
+		*/
+		value: {
+			type: String,
+			default: 'floating',
+		},
+	},
+	computed: {
+		placeholder() {
+			return t('Type to search timezone')
+		},
+		selectedTimezone() {
+			for (const additionalTimezone of this.additionalTimezones) {
+				if (additionalTimezone.timezoneId === this.value) {
+					return additionalTimezone
+				}
+			}
+
+			return {
+				label: getReadableTimezoneName(this.value),
+				timezoneId: this.value,
+			}
+		},
+		options() {
+			const timezoneManager = getTimezoneManager()
+			return getSortedTimezoneList(timezoneManager.listAllTimezones(), this.additionalTimezones)
+		},
+	},
+	methods: {
+		change(newValue) {
+			if (!newValue) {
+				return
+			}
+
+			/**
+			 * Two-way binding of the value prop. Use v-model="selectedTimezone" for two-way binding
+			 */
+			this.$emit('input', newValue.timezoneId)
+		},
+	},
+}
+</script>
---
+++ src/components/TimezonePicker/timezoneDataProviderService.js
@@ -0,0 +1,69 @@
+/**
+ * @copyright Copyright (c) 2019 Georg Ehrke
+ *
+ * @author Georg Ehrke <oc.list@georgehrke.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+import { getTimezoneManager } from 'calendar-js'
+
+import tzData from '../../../resources/timezones/zones.json'
+import logger from '../../utils/logger.js'
+
+const timezoneManager = getTimezoneManager()
+let initialized = false
+
+/**
+ * Gets the timezone-manager
+ * initializes it if necessary
+ *
+ * @returns {TimezoneManager}
+ */
+export default function() {
+	if (!initialized) {
+		initialize()
+	}
+
+	return timezoneManager
+}
+
+/**
+ * Initializes the timezone-manager with all timezones shipped by the calendar app
+ */
+function initialize() {
+	logger.debug(`Using version ${tzData.version} of the timezone database`)
+
+	for (const tzid in tzData.zones) {
+		if (Object.prototype.hasOwnProperty.call(tzData.zones, [tzid])) {
+			const ics = [
+				'BEGIN:VTIMEZONE',
+				'TZID:' + tzid,
+				...tzData.zones[tzid].ics,
+				'END:VTIMEZONE',
+			].join('\r\n')
+			timezoneManager.registerTimezoneFromICS(tzid, ics)
+		}
+	}
+
+	for (const tzid in tzData.aliases) {
+		if (Object.prototype.hasOwnProperty.call(tzData.aliases, [tzid])) {
+			timezoneManager.registerAlias(tzid, tzData.aliases[tzid].aliasTo)
+		}
+	}
+
+	initialized = true
+}
