b65ca8e4b8e4480c311dd460aac8d84e3714a908
Raimund Schlüßler <raimund.schluessler@mailbox.org>
2022-03-20 22:21:32 +0100

    Debug

--- src/components/Breadcrumbs/Breadcrumbs.vue
+++ src/components/Breadcrumbs/Breadcrumbs.vue
@@ -590,8 +590,6 @@ export default {
 	}
 
 	& #{&}__crumbs {
-		flex-shrink: 1;
-		max-width: 100%;
 		/**
 		 * This value is given by the min-width of the last crumb (100px) plus
 		 * two times the width of a crumb with an icon (first crumb and hidden crumbs actions).
