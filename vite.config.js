ea85cf895a4b5803e72dd8aa9e46419754aab1a5
Vinicius Reis <vinicius.reis@nextcloud.com>
2022-08-10 15:35:27 -0300

    🩹 prevent any external package to be bundled
    Signed-off-by: Vinicius Reis <vinicius.reis@nextcloud.com>

--- vite.config.js
+++ vite.config.js
@@ -19,24 +19,17 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  */
-import { fileURLToPath, URL } from 'node:url'
 import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue2'
 import commonjs from 'vite-plugin-commonjs'
 import md5 from 'md5'
-import { dependencies, version } from './package.json'
+import { version } from './package.json'
 import { loadTranslations } from './resources/translations.js'
+import excludeDependenciesFromBundle from 'rollup-plugin-exclude-dependencies-from-bundle'
 
 const versionHash = md5(version).slice(0, 7)
 const SCOPE_VERSION = JSON.stringify(versionHash)
-
-const __dirname = fileURLToPath(new URL('.', import.meta.url))
-
-const external = [
-	...Object.keys(dependencies),
-	/vue-material-design-icons\/.*/,
-]
 
 const vueDoc = {
 	name: 'vue-doc',
@@ -58,8 +51,10 @@ export default async () => {
 			SCOPE_VERSION,
 			TRANSLATIONS: JSON.stringify(translations),
 		},
-		alias: {
-			'@': resolve(__dirname, './src'),
+		resolve: {
+			alias: {
+				'@': resolve(__dirname, './src'),
+			},
 		},
 		css: {
 			preprocessorOptions: {
@@ -80,7 +75,7 @@ export default async () => {
 				formats: ['es', 'cjs'],
 			},
 			rollupOptions: {
-				external,
+				plugins: [excludeDependenciesFromBundle()],
 				output: {
 					globals: { vue: 'Vue' },
 				},
