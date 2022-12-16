aba26fc7321031bdec964dab38040f5863d8f219
Ferdinand Thiessen <rpm@fthiessen.de>
2022-12-16 16:30:28 +0100

    Add `NcSavingIndicatorIcon`
    An icon to indicate the saving state of a document, file... as used by
    the *text* and the *office* app.
    The icon is based on the material design icon `Check` combined with `CircleMedium`.
    
    Signed-off-by: Ferdinand Thiessen <rpm@fthiessen.de>

---
+++ src/components/NcSavingIndicatorIcon/NcSavingIndicatorIcon.vue
@@ -0,0 +1,130 @@
+<!--
+  - @copyright Copyright (c) 2022
+  -
+  - @license GNU AGPL version 3 or any later version
+  -
+  - This program is free software: you can redistribute it and/or modify
+  - it under the terms of the GNU Affero General Public License as
+  - published by the Free Software Foundation, either version 3 of the
+  - License, or (at your option) any later version.
+  -
+  - This program is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  - GNU Affero General Public License for more details.
+  -
+  - You should have received a copy of the GNU Affero General Public License
+  - along with this program. If not, see <http://www.gnu.org/licenses/>.
+  -
+  -->
+
+<docs>
+# Usage
+
+```
+<template>
+<div>
+	<div>
+		<NcSavingIndicatorIcon :title="`${savingState === 'error' ? 'Error saving' : savingState} file`"
+			:error="savingState === 'error'"
+			:saving="savingState === 'saving'" />
+	</div>
+	<br />
+	<div style="display: flex; justify-content: space-around;">
+		<NcCheckboxRadioSwitch :checked.sync="savingState"
+			value="saved"
+			name="saving_radio"
+			type="radio">
+			Saved
+		</NcCheckboxRadioSwitch>
+		<NcCheckboxRadioSwitch :checked.sync="savingState"
+			value="saving"
+			name="saving_radio"
+			type="radio">
+			Saving
+		</NcCheckboxRadioSwitch>
+		<NcCheckboxRadioSwitch :checked.sync="savingState"
+			value="error"
+			name="saving_radio"
+			type="radio">
+			Error
+		</NcCheckboxRadioSwitch>
+	</div>
+</div>
+</template>
+<script>
+export default {
+	data() {
+		return {
+			savingState: 'saved',
+		}
+	},
+}
+</script>
+
+```
+</docs>
+
+<template>
+	<span :aria-label="title"
+		class="material-design-icon"
+		role="img"
+		@click="$emit('click', $event)">
+		<svg :fill="fillColor"
+			class="material-design-icon__svg"
+			:width="size"
+			:height="size"
+			viewBox="0 0 24 24">
+			<path :fill="indicatorColor" d="m19 15a4 4 0 0 0-4 4 4 4 0 0 0 4 4 4 4 0 0 0 4-4 4 4 0 0 0-4-4z" />
+			<path fill="currentColor" d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z">
+				<title v-if="title">{{ title }}</title>
+			</path>
+		</svg>
+	</span>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	name: 'NcSavingIndicatorIcon',
+	props: {
+		/**
+		 * Specify the size of the saving icon.
+		 */
+		size: {
+			type: Number,
+			default: 20,
+		},
+		/**
+		 * Specify what is saved.
+		 */
+		title: {
+			type: String,
+			default: '',
+		},
+		/**
+		 * Set to true when saving is in progress.
+		 */
+		saving: {
+			type: Boolean,
+			default: false,
+			required: false,
+		},
+		/**
+		 * Set to true if an error occured while saving.
+		 */
+		error: {
+			type: Boolean,
+			default: false,
+			required: false,
+		},
+	},
+	emits: ['click'],
+	computed: {
+		indicatorColor() {
+			return this.error ? 'var(--color-error)' : (this.saving ? 'var(--color-primary)' : 'none')
+		},
+	},
+})
+</script>
