458626641f47757e13f19868ff995b263930a9ac
Raimund Schlüßler <raimund.schluessler@mailbox.org>
2023-01-31 17:21:06 +0100

    Adjust getGettextBuilder import
    Signed-off-by: Raimund Schlüßler <raimund.schluessler@mailbox.org>

--- src/l10n.js
+++ src/l10n.js
@@ -1,4 +1,4 @@
-import { getGettextBuilder } from '@nextcloud/l10n/dist/gettext.js'
+import { getGettextBuilder } from '@nextcloud/l10n/gettext'
 
 const gtBuilder = getGettextBuilder()
 	.detectLocale()
