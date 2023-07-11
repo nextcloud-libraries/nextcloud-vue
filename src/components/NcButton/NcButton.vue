7d1a4559123871b9450c6f4970822e231814e06a
Ferdinand Thiessen <opensource@fthiessen.de>
2023-07-11 17:59:37 +0200

    chore: Update `@nextcloud/stylelint-config` and fix issues detected
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- src/components/NcButton/NcButton.vue
+++ src/components/NcButton/NcButton.vue
@@ -540,7 +540,6 @@ export default {
 		color: var(--color-main-text);
 		background-color: transparent;
 		&:hover:not(:disabled) {
-			background-color: var(--color);
 			background-color: var(--color-background-hover);
 		}
 	}
