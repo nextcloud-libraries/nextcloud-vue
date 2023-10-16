bff4e9d4c1f0a1f948d70e92488fe58bdfab75fe
Christopher Ng <chrng8@gmail.com>
2023-10-16 16:27:20 -0700

    fix(NcActions): Remove aria-expanded when closed
    Signed-off-by: Christopher Ng <chrng8@gmail.com>

--- src/components/NcActions/NcActions.vue
+++ src/components/NcActions/NcActions.vue
@@ -1074,6 +1074,15 @@ export default {
 						},
 					})
 			)
+			const ariaExpandedForTrigger = () => {
+				if (isNav) {
+					return this.opened.toString()
+				}
+				if (this.opened) {
+					return this.opened.toString()
+				}
+				return null
+			}
 			return h('NcPopover',
 				{
 					ref: 'popover',
@@ -1120,7 +1129,7 @@ export default {
 							'aria-haspopup': isNav ? null : 'menu',
 							'aria-label': this.menuName ? null : this.ariaLabel,
 							'aria-controls': this.opened ? this.randomId : null,
-							'aria-expanded': this.opened.toString(),
+							'aria-expanded': ariaExpandedForTrigger(),
 						},
 						on: {
 							focus: this.onFocus,
