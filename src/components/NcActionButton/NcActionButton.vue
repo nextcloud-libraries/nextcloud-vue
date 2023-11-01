557629c395d18c3bf61904b576ac1d189ba79946
Ferdinand Thiessen <opensource@fthiessen.de>
2023-11-02 00:39:22 +0100

    feat(NcActionButton): Allow pressed state on NcActionButton - similar to NcButton
    * Use trailing icon for pressed state
    
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- src/components/NcActionButton/NcActionButton.vue
+++ src/components/NcActionButton/NcActionButton.vue
@@ -170,13 +170,55 @@ export default {
 }
 </script>
 ```
+
+You can set an "pressed" state, for example you have a toggle button, in this example the "fullscreen" button:
+
+```vue
+<template>
+	<NcActions>
+		<NcActionButton :pressed="handRaised" @click="handRaised = !handRaised">
+			<template #icon>
+				<HandBackLeft :size="20" />
+			</template>
+			Raise hand
+		</NcActionButton>
+		<NcActionButton :pressed="fullscreen" @click="fullscreen = !fullscreen">
+			<template #icon>
+				<Fullscreen :size="20" />
+			</template>
+			Fullscreen
+		</NcActionButton>
+	</NcActions>
+</template>
+<script>
+import HandBackLeft from 'vue-material-design-icons/HandBackLeft.vue'
+import Fullscreen from 'vue-material-design-icons/Fullscreen.vue'
+
+export default {
+	components: {
+		HandBackLeft,
+		Fullscreen,
+	},
+	data() {
+		return {
+			fullscreen: true,
+			handRaised: false,
+		}
+	},
+}
+</script>
+```
 </docs>
 
 <template>
 	<li class="action" :class="{ 'action--disabled': disabled }" :role="isInSemanticMenu && 'presentation'">
 		<button class="action-button button-vue"
-			:class="{ focusable: isFocusable }"
+			:class="{
+				'action-button--active': pressed,
+				focusable: isFocusable,
+			}"
 			:aria-label="ariaLabel"
+			:aria-pressed="active"
 			:title="title"
 			:role="isInSemanticMenu && 'menuitem'"
 			type="button"
@@ -212,7 +254,9 @@ export default {
 			<span v-else class="action-button__text">{{ text }}</span>
 
 			<!-- right arrow icon when there is a sub-menu -->
-			<ChevronRightIcon v-if="isMenu" class="action-button__menu-icon" />
+			<ChevronRightIcon v-if="isMenu" :size="20" class="action-button__menu-icon" />
+			<CheckIcon v-else-if="pressed === true" :size="20" class="action-button__pressed-icon" />
+			<span v-else-if="pressed === false" class="action-button__pressed-icon material-design-icon" />
 
 			<!-- fake slot to gather inner text -->
 			<slot v-if="false" />
@@ -221,6 +265,7 @@ export default {
 </template>
 
 <script>
+import CheckIcon from 'vue-material-design-icons/Check.vue'
 import ChevronRightIcon from 'vue-material-design-icons/ChevronRight.vue'
 import ActionTextMixin from '../../mixins/actionText.js'
 
@@ -231,6 +276,7 @@ export default {
 	name: 'NcActionButton',
 
 	components: {
+		CheckIcon,
 		ChevronRightIcon,
 	},
 	mixins: [ActionTextMixin],
@@ -244,21 +290,21 @@ export default {
 
 	props: {
 		/**
+		 * @deprecated To be removed in @nextcloud/vue 9. Migration guide: remove ariaHidden prop from NcAction* components.
+		 * @todo Add a check in @nextcloud/vue 9 that this prop is not provided,
+		 * otherwise root element will inherit incorrect aria-hidden.
+		 */
+		 ariaHidden: {
+			type: Boolean,
+			default: null,
+		},
+
+		/**
 		 * disabled state of the action button
 		 */
 		disabled: {
 			type: Boolean,
 			default: false,
-		},
-
-		/**
-		 * @deprecated To be removed in @nextcloud/vue 9. Migration guide: remove ariaHidden prop from NcAction* components.
-		 * @todo Add a check in @nextcloud/vue 9 that this prop is not provided,
-		 * otherwise root element will inherit incorrect aria-hidden.
-		 */
-		ariaHidden: {
-			type: Boolean,
-			default: null,
 		},
 
 		/**
@@ -268,6 +314,15 @@ export default {
 		isMenu: {
 			type: Boolean,
 			default: false,
+		},
+
+		/**
+		 * The pressed state of the button if it has a checked state
+		 * This will add the `aria-pressed` attribute and for the button to have a primary border in checked state.
+		 */
+		 pressed: {
+			type: Boolean,
+			default: null,
 		},
 	},
 
@@ -289,4 +344,9 @@ export default {
 @include action-active;
 @include action--disabled;
 @include action-item('button');
+
+.action-button__pressed-icon {
+	margin-left: auto;
+	margin-right: -$icon-margin;
+}
 </style>
