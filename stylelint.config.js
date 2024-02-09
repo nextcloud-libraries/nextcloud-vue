1749aaced233f795b745de992c52e6a88ce001c5
Grigorii K. Shartsev <me@shgk.me>
2024-02-09 17:58:13 +0100

    chore(stylelint): add CSS Modules `:global` support
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,3 +1,15 @@
 const stylelintConfig = require('@nextcloud/stylelint-config')
 
-module.exports = stylelintConfig
+module.exports = {
+	extends: ['@nextcloud/stylelint-config'],
+	rules: {
+		// For CSS Modules
+		// If there will be more rules for CSS Modules - consider extending stylelint-config-css-modules
+		'selector-pseudo-class-no-unknown': [
+			true,
+			{
+				ignorePseudoClasses: [...stylelintConfig.rules['selector-pseudo-class-no-unknown'][1].ignorePseudoClasses, 'global'],
+			},
+		],
+	},
+}
