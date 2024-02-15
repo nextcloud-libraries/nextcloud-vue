3b97bf1132b6bb7f994db61e0bf5eec8ee6e1cdc
greta <gretadoci@gmail.com>
2024-02-15 17:22:27 +0100

    Fix timezone dropdown
    Signed-off-by: greta <gretadoci@gmail.com>

--- src/components/NcDateTimePicker/NcDateTimePicker.vue
+++ src/components/NcDateTimePicker/NcDateTimePicker.vue
@@ -497,6 +497,6 @@ export default ScopeComponent({
 // TODO: This should be scoped or targeted by a specific selector, but the NcSelect component does not allow this yet.
 .vs__dropdown-menu--floating {
 	// Higher z-index than the popover in which the NcSelect is located.
-	z-index: 100001;
+	z-index: 100001 !important;
 }
 </style>
