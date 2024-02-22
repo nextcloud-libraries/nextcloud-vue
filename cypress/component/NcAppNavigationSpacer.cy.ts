cef2ee48119aa484f67c434e95bb3002ec431d15
Ferdinand Thiessen <opensource@fthiessen.de>
2024-02-22 01:10:48 +0100

    fix(NcAppNavigationSpacer): Adjust order to make the spacer work again
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

---
+++ cypress/component/NcAppNavigationSpacer.cy.ts
@@ -0,0 +1,26 @@
+import { h } from 'vue'
+
+import NcAppNavigation from '../../src/components/NcAppNavigation/NcAppNavigation.vue'
+import NcAppNavigationItem from '../../src/components/NcAppNavigationItem/NcAppNavigationItem.vue'
+import NcAppNavigationSpacer from '../../src/components/NcAppNavigationSpacer/NcAppNavigationSpacer.vue'
+
+describe('NcAppNavigationSpacer', () => {
+	it('works', () => {
+		cy.mount({
+			render: () => h(NcAppNavigation, null, {
+				list: () => [
+					h(NcAppNavigationItem, { name: 'First' }),
+					h(NcAppNavigationSpacer),
+					h(NcAppNavigationItem, { name: 'Second' }),
+				],
+			}),
+		})
+
+		cy.contains('li', 'First').should('exist').then(($first) => {
+			cy.contains('li', 'Second').should('exist').then(($second) => {
+				// Check that the second element is at least 22px below the first one (thats our spacer)
+				expect($second.offset()!.top - 22).gte($first.offset()!.top + $first.height()!)
+			})
+		})
+	})
+})
