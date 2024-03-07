d9b431f9957557ec5c096fe232acd4ba43ea452b
Grigorii K. Shartsev <me@shgk.me>
2024-03-07 20:35:58 +0100

    fix(NcAppSidebar): remove always hover styles from actions in non-compact mode
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- src/components/NcAppSidebar/NcAppSidebar.vue
+++ src/components/NcAppSidebar/NcAppSidebar.vue
@@ -1178,7 +1178,6 @@ $top-buttons-spacing: 6px;
 						height: $clickable-area;
 						width: $clickable-area;
 						border-radius: math.div($clickable-area, 2);
-						background-color: $action-background-hover;
 						margin-left: 5px;
 					}
 				}
