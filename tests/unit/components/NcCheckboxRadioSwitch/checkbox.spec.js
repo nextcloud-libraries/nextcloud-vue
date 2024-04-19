395fc5f235be9de0bd4ab4a80a89511fcc7245b6
Ferdinand Thiessen <opensource@fthiessen.de>
2024-04-20 01:54:10 +0200

    fix(NcCheckboxRadioSwitch): Pass attrs to `input` if available
    If not button type, pass the attrs to the input element to allow setting aria tags like `aria-invalid` and `aria-errormessage`
    
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

---
+++ tests/unit/components/NcCheckboxRadioSwitch/checkbox.spec.js
@@ -0,0 +1,52 @@
+/**
+ * @copyright Copyright (c) 2024 Ferdinand Thiessen <opensource@fthiessen.de>
+ *
+ * @author Ferdinand Thiessen <opensource@fthiessen.de>
+ *
+ * @license AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+import { shallowMount } from '@vue/test-utils'
+import NcCheckboxRadioSwitch from '../../../../src/components/NcCheckboxRadioSwitch/NcCheckboxRadioSwitch.vue'
+
+describe('NcCheckboxRadioSwitch', () => {
+	it('sets text content', () => {
+		const wrapper = shallowMount(NcCheckboxRadioSwitch, {
+			slots: {
+				default: 'Test',
+			},
+		})
+
+		expect(wrapper.text()).toContain('Test')
+	})
+
+	it('forwards aria-invalid and aria-errormessage to input', () => {
+		const wrapper = shallowMount(NcCheckboxRadioSwitch, {
+			slots: {
+				default: 'Test',
+			},
+			attrs: {
+				'aria-invalid': 'true',
+				'aria-errormessage': 'id-test',
+			},
+		})
+
+		const input = wrapper.find('input')
+		expect(input.attributes('aria-invalid')).toBe('true')
+		expect(input.attributes('aria-errormessage')).toBe('id-test')
+	})
+})
