f0145a2e796bf54e6d91c534e634815825205324
Grigorii K. Shartsev <me@shgk.me>
2024-06-04 16:06:12 +0200

    chore(NcVNodes): add docs
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- src/components/NcVNodes/NcVNodes.vue
+++ src/components/NcVNodes/NcVNodes.vue
@@ -20,6 +20,53 @@
  -
  -->
 
+<docs>
+A util component to render a VNode or an array of VNodes from the prop or the default slot.
+
+### Render the given or generated VNodes
+
+```vue
+<template>
+	<NcVNodes :vnodes="renderDiv()" />
+</template>
+
+<script>
+export default {
+	methods: {
+		renderDiv() {
+			return this.$createElement('div', 'This div was created programmatically as a VNode')
+		},
+	}
+}
+</script>
+```
+
+### Render content in a conditional wrapper
+
+```vue
+<template>
+	<div>
+		<p>
+			<NcCheckboxRadioSwitch :checked.sync="shouldRenderNcNoteCard">Render text inside "NcNoteCard"</NcCheckboxRadioSwitch>
+		</p>
+		<component :is="shouldRenderNcNoteCard ? 'NcNoteCard' : 'NcVNodes'">
+			<p>This content is rendered {{ shouldRenderNcNoteCard ? 'with' : 'without' }} a "NcNoteCard" wrapper</p>
+		</component>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			shouldRenderNcNoteCard: true,
+		}
+	},
+}
+</script>
+```
+</docs>
+
 <script>
 export default {
 	name: 'NcVNodes',
@@ -39,6 +86,8 @@ export default {
 	 * @return {object} The created VNode
 	 */
 	render(h) {
+		// eslint-disable-next-line jsdoc/check-tag-names
+		/** @slot Directly rendered content */
 		return this.vnodes || this.$slots?.default || this.$scopedSlots?.default?.()
 	},
 }
