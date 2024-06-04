f8ecd7fc7b77d8a5d7764a39051acf5cd479324b
Grigorii K. Shartsev <me@shgk.me>
2024-06-04 16:06:12 +0200

    chore(NcVNodes): add docs
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- src/components/NcVNodes/NcVNodes.vue
+++ src/components/NcVNodes/NcVNodes.vue
@@ -20,6 +20,55 @@
  -
  -->
 
+<docs>
+A util component to render a VNode or an array of VNodes from the prop or the default slot.
+
+### Render the given or generated VNodes
+
+```vue
+<template>
+	<NcVNodes :vnodes="renderDiv()" />
+</template>
+
+<script>
+import { h } from 'vue'
+
+export default {
+	methods: {
+		renderDiv() {
+			return h('div', 'This div was created programmatically as a VNode')
+		},
+	}
+}
+</script>
+```
+
+### Render content in a conditional wrapper
+
+```vue
+<template>
+	<div>
+		<p>
+			<NcCheckboxRadioSwitch v-model="shouldRenderNcNoteCard">Render text inside "NcNoteCard"</NcCheckboxRadioSwitch>
+		</p>
+		<component :is="shouldRenderNcNoteCard ? 'NcNoteCard' : 'NcVNodes'">
+			<p>This content is rendered {{ shouldRenderNcNoteCard ? 'with' : 'without' }} a "NcNoteCard" wrapper</p>
+		</component>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			shouldRenderNcNoteCard: true,
+		}
+	},
+}
+</script>
+```
+</docs>
+
 <script>
 export default {
 	name: 'NcVNodes',
