7ea01b84e8ff50865efa0770f1ab0cd56a9e8390
Maksim Sukharev <antreesy.web@gmail.com>
2024-08-02 15:29:44 +0200

    style(NcAvatar): make border-radius consistent
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/components/NcAvatar/NcAvatar.vue
+++ src/components/NcAvatar/NcAvatar.vue
@@ -772,8 +772,9 @@ export default {
 				min-width: var(--size);
 			}
 		}
-		& > :deep(.button-vue) {
-			--button-radius: calc(var(--button-size) / 2);
+		& > :deep(.button-vue),
+		& > :deep(.action-item .button-vue) {
+			--button-radius: calc(var(--size) / 2);
 		}
 	}
 
@@ -782,7 +783,7 @@ export default {
 		height: var(--size);
 		width: var(--size);
 		background-color: var(--color-main-background);
-		border-radius: 50%;
+		border-radius: calc(var(--size) / 2);
 
 		.avatardiv__initials {
 			position: absolute;
@@ -851,7 +852,7 @@ export default {
 
 .avatar-class-icon {
 	display: block;
-	border-radius: 50%;
+	border-radius: calc(var(--size) / 2);
 	background-color: var(--color-background-darker);
 	height: 100%;
 }
