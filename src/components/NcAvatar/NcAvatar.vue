c679820919e8a4c4c6dd9f58a9691fcf84889232
Maksim Sukharev <antreesy.web@gmail.com>
2024-08-14 10:52:21 +0200

    chore(docs): extend NcAvatar docs with different status icon and sizes
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/components/NcAvatar/NcAvatar.vue
+++ src/components/NcAvatar/NcAvatar.vue
@@ -20,7 +20,7 @@
 ### Avatar with material design icon
 
 ```
- <template>
+<template>
 	<NcAvatar>
 		<template #icon>
 			<AccountMultiple :size="20" />
@@ -34,32 +34,93 @@ export default {
 	components: {
 		AccountMultiple,
 	},
- }
- </script>
+}
+</script>
 ```
 
 ### Avatar with preloaded status
 ```
- <template>
+<template>
+<div class="grid">
 	<NcAvatar user="janedoe"
 		display-name="Jane Doe"
-		:preloaded-user-status="status">
+		:size="44"
+		:preloaded-user-status="status.online">
 	</NcAvatar>
+	<NcAvatar user="janedoe"
+		display-name="Jane Doe"
+		:size="44"
+		:preloaded-user-status="status.away">
+	</NcAvatar>
+	<NcAvatar user="janedoe"
+		display-name="Jane Doe"
+		:size="44"
+		:preloaded-user-status="status.dnd">
+	</NcAvatar>
+	<NcAvatar user="janedoe"
+		display-name="Jane Doe"
+		:size="44"
+		:preloaded-user-status="status.custom">
+	</NcAvatar>
+	<NcAvatar user="janedoe"
+		display-name="Jane Doe"
+		:preloaded-user-status="status.online">
+	</NcAvatar>
+	<NcAvatar user="janedoe"
+		display-name="Jane Doe"
+		:preloaded-user-status="status.away">
+	</NcAvatar>
+	<NcAvatar user="janedoe"
+		display-name="Jane Doe"
+		:preloaded-user-status="status.dnd">
+	</NcAvatar>
+	<NcAvatar user="janedoe"
+		display-name="Jane Doe"
+		:preloaded-user-status="status.custom">
+	</NcAvatar>
+</div>
 </template>
-<script>
 
+<script>
 export default {
 	data() {
 		return {
 			status: {
-				icon: '',
-				status: 'dnd',
-				message: 'Not busy',
+				online: {
+					icon: '',
+					status: 'online',
+					message: 'Available',
+				},
+				away: {
+					icon: '',
+					status: 'away',
+					message: 'Away',
+				},
+				dnd: {
+					icon: '',
+					status: 'dnd',
+					message: 'Busy',
+				},
+				custom: {
+					icon: '📆',
+					status: 'online',
+					message: 'In a meeting',
+				}
 			},
 		}
 	},
- }
- </script>
+}
+</script>
+<style>
+	.grid {
+		width: fit-content;
+		display: grid;
+		justify-content: center;
+		align-items: center;
+		gap: 8px;
+		grid-template-columns: repeat(4, 1fr);
+	}
+</style>
 ```
 
 ### Avatar for non-users
