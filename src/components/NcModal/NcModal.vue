616dadc873948cd01c344f470a9a9d60a9a2c0f9
Grigorii K. Shartsev <me@shgk.me>
2024-08-18 21:57:23 +0200

    fix(NcModal): use --header-height instead of const
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- src/components/NcModal/NcModal.vue
+++ src/components/NcModal/NcModal.vue
@@ -853,7 +853,7 @@ export default {
 	align-items: center;
 	justify-content: center;
 	width: 100%;
-	height: $header-height;
+	height: var(--header-height);
 	overflow: hidden;
 	transition: opacity 250ms, visibility 250ms;
 
@@ -889,14 +889,14 @@ export default {
 			align-items: center;
 			justify-content: center;
 			box-sizing: border-box;
-			margin: calc(calc($header-height - var(--default-clickable-area)) / 2);
+			margin: calc(calc(var(--header-height) - var(--default-clickable-area)) / 2);
 			padding: 0;
 		}
 
 		.play-pause-icons {
 			position: relative;
-			width: $header-height;
-			height: $header-height;
+			width: var(--header-height);
+			height: var(--header-height);
 			margin: 0;
 			padding: 0;
 			cursor: pointer;
@@ -916,14 +916,14 @@ export default {
 				box-sizing: border-box;
 				width: var(--default-clickable-area);
 				height: var(--default-clickable-area);
-				margin: calc(calc($header-height - var(--default-clickable-area)) / 2);
+				margin: calc(calc(var(--header-height) - var(--default-clickable-area)) / 2);
 				cursor: pointer;
 				opacity: $opacity_normal;
 			}
 		}
 
 		&:deep() .action-item {
-			margin: calc(calc($header-height - var(--default-clickable-area)) / 2);
+			margin: calc(calc(var(--header-height) - var(--default-clickable-area)) / 2);
 
 			&--single {
 				box-sizing: border-box;
@@ -1014,7 +1014,7 @@ export default {
 
 	// We allow 90% max-height, but we need to ensure the header does not overlap the modal
 	// as the modal is centered, we need the space on top and bottom
-	$max-modal-height: min(90%, calc(100% - 2 * $header-height));
+	$max-modal-height: min(90%, calc(100% - 2 * var(--header-height)));
 
 	// Sizing
 	&--small {
@@ -1043,7 +1043,7 @@ export default {
 			width: 100%;
 			height: calc(100% - var(--header-height));
 			position: absolute;
-			top: $header-height;
+			top: var(--header-height);
 			border-radius: 0;
 		}
 	}
@@ -1056,7 +1056,7 @@ export default {
 			max-height: initial;
 			height: calc(100% - var(--header-height));
 			position: absolute;
-			top: $header-height;
+			top: var(--header-height);
 			border-radius: 0;
 		}
 	}
