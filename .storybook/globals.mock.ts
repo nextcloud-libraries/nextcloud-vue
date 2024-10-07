991dc024f86be18551ffc3230bb78f39a9cb76a8
Grigorii K. Shartsev <me@shgk.me>
2024-10-07 13:52:31 +0200

    docs: add documentation using storybook

---
+++ .storybook/globals.mock.ts
@@ -0,0 +1,44 @@
+/**
+ * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+// Global variables
+window._oc_webroot = ''
+
+window.OC = {
+	debug: true,
+	getCurrentUser() {
+		return {
+			uid: 'admin',
+			displayName: 'Administrator',
+		}
+	},
+	generateUrl() {
+		return 'https://raw.githubusercontent.com/nextcloud/promo/master/nextcloud-icon.png'
+	},
+	getLanguage() {
+		return 'en'
+	},
+	isUserAdmin() {
+		return true
+	},
+	config: {},
+	coreApps: [
+		'',
+		'admin',
+		'log',
+		'core/search',
+		'core',
+		'3rdparty',
+	],
+	appswebroots: {
+		calendar: '/apps/calendar',
+		deck: '/apps/deck',
+		files: '/apps/files',
+		spreed: '/apps/spreed',
+	},
+	webroot: '',
+}
+window.OCA = {}
+window.appName = 'nextcloud-vue'
