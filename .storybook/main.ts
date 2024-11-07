0c84d2b2bfcb6c14e80c59abdab3d0883437afea
Grigorii K. Shartsev <me@shgk.me>
2024-11-07 23:04:29 +0100

    docs: prefix styles for storybook to not mess with storybook styles

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,24 @@
 import type { StorybookConfig } from '@storybook/vue3-vite'
+import { join } from 'path'
 
 const config: StorybookConfig = {
 	stories: ['../docs/**/*.mdx', '../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 
 	addons: [
-    	// Collection of essential addons like actions, controls, backgrounds, viewport, etc.
+		// Collection of essential addons like actions, controls, backgrounds, viewport, etc.
 		'@storybook/addon-essentials',
-    	// Links between stories
+		// Links between stories
 		'@storybook/addon-links',
-    	// Allows to see and edit the source code of a story
+		// Allows to see and edit the source code of a story
 		'@storybook/addon-storysource',
 	],
 
 	framework: {
 		name: '@storybook/vue3-vite',
 		options: {
+			builder: {
+				viteConfigPath: join(__dirname, 'vite.config.ts'),
+			},
 			// vue-component-meta is recommended for Vue 3 and will be the default in the future
 			docgen: 'vue-component-meta',
 		},
