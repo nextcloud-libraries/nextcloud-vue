f62f92161fd242d9de2b1dc4b513976645cba7aa
Maksim Sukharev <antreesy.web@gmail.com>
2024-11-19 17:09:42 +0100

    fix(test): ensure parsed content after rendering matches the original
    - sanitized strings are skipped
    
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- tests/unit/mixins/richEditor.spec.js
+++ tests/unit/mixins/richEditor.spec.js
@@ -29,6 +29,9 @@ describe('richEditor.js', () => {
 			const output = editor.vm.renderContent(input)
 
 			expect(output).toEqual('hard<br>break')
+
+			const parsedOutput = editor.vm.parseContent(output)
+			expect(parsedOutput).toEqual(input)
 		})
 
 		it('no duplicated ampersand (from Linkify)', () => {
@@ -37,6 +40,9 @@ describe('richEditor.js', () => {
 			const output = editor.vm.renderContent(input)
 
 			expect(output).toEqual('hello &amp;')
+
+			const parsedOutput = editor.vm.parseContent(output)
+			expect(parsedOutput).toEqual(input)
 		})
 
 		it('keeps mentions without user data', () => {
@@ -45,6 +51,9 @@ describe('richEditor.js', () => {
 			const output = editor.vm.renderContent(input)
 
 			expect(output).toEqual('hello @foobar')
+
+			const parsedOutput = editor.vm.parseContent(output)
+			expect(parsedOutput).toEqual(input)
 		})
 
 		it('keeps mentions with user data', () => {
@@ -66,6 +75,9 @@ describe('richEditor.js', () => {
 			expect(output).toMatch(/^hello <span.+role="heading" title="J. Doe".+\/span>!<br>how are you\?$/)
 			expect(output).not.toMatch(/[\n\t]/gmi)
 			expect(output).not.toMatch(/>\s+</g)
+
+			const parsedOutput = editor.vm.parseContent(output)
+			expect(parsedOutput).toEqual(input)
 		})
 
 		it('keeps adjacent mentions with user data', () => {
@@ -91,6 +103,9 @@ describe('richEditor.js', () => {
 			const output = editor.vm.renderContent(input)
 
 			expect(output).toMatch(/^hello <span.+role="heading" title="J. Doe".+\/span> <span.+role="heading" title="J. Guest".+\/span>! how are you\?$/)
+
+			const parsedOutput = editor.vm.parseContent(output)
+			expect(parsedOutput).toEqual(input)
 		})
 
 		it('keep mentions with special characters', () => {
@@ -119,6 +134,9 @@ describe('richEditor.js', () => {
 			for (const i in inputs) {
 				const output = editor.vm.renderContent(inputs[i])
 				expect(output).toEqual(outputs[i])
+
+				const parsedOutput = editor.vm.parseContent(output)
+				expect(parsedOutput).toEqual(inputs[i])
 			}
 		})
 	})
