2a3b2edf5ac07c3256a70ca829c491ab2014e687
Grigorii K. Shartsev <me@shgk.me>
2025-02-28 13:33:32 +0100

    chore: add useTrapStackControl util composable
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- src/utils/focusTrap.ts
+++ src/utils/focusTrap.ts
@@ -20,3 +20,37 @@ export function getTrapStack() {
 
 	return window._nc_focus_trap
 }
+
+/**
+ * Create controller to pause/unpause the global focus trap stack.
+ *
+ * When some component manually implements its own focus-trap without using global focus-trap stack,
+ * it also needs to pause the global stack to avoid conflict.
+ */
+export function createTrapStackController() {
+	/**
+	 * Stack of focus traps that were paused to unpause the exact stack later
+	 */
+	let pausedStack: FocusTrap[] = []
+
+	return {
+		/**
+		 * Pause the current focus-trap stack
+		 */
+		pause() {
+			pausedStack = [...getTrapStack()]
+			for (const trap of pausedStack) {
+				trap.pause()
+			}
+		},
+		/**
+		 * Unpause the paused focus trap stack
+		 */
+		unpause() {
+			for (const trap of pausedStack) {
+				trap.unpause()
+			}
+			pausedStack = []
+		},
+	}
+}
