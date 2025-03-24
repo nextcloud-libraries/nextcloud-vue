5ca2a5fb36128627e24d5e142581e3c8d209509a
Ferdinand Thiessen <opensource@fthiessen.de>
2025-03-24 16:59:41 +0100

    fix(NcInputField): Only show error state border if user interacted with the input
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- src/components/NcInputField/NcInputField.vue
+++ src/components/NcInputField/NcInputField.vue
@@ -425,7 +425,7 @@ export default {
 		}
 
 		&--error,
-		&:invalid {
+		&:user-invalid {
 			border-color: var(--color-error) !important; //Override hover border color
 			&:focus-visible {
 				box-shadow: rgb(248, 250, 252) 0px 0px 0px 2px, var(--color-primary-element) 0px 0px 0px 4px, rgba(0, 0, 0, 0.05) 0px 1px 2px 0px
