132b1edd6c0da69d683afc2946de3c685529a1e4
Ferdinand Thiessen <opensource@fthiessen.de>
2025-03-28 10:55:20 +0100

    refactor(useIsFullscreen): migrate to Typescript
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- src/composables/index.js
+++ src/composables/index.js
@@ -3,8 +3,8 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-export * from './useIsFullscreen/index.js'
-export * from './useIsMobile/index.js'
 export * from './useFormatDateTime.ts'
 export * from './useHotKey/index.ts'
 export * from './useIsDarkTheme/index.ts'
+export * from './useIsFullscreen/index.ts'
+export * from './useIsMobile/index.js'
