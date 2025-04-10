22d6286131411bca02e3844b3a96312e934eaea5
Ferdinand Thiessen <opensource@fthiessen.de>
2025-04-10 13:57:57 +0200

    fix(useIsDarkTheme): respect overwritten theme
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- src/composables/useIsDarkTheme/index.ts
+++ src/composables/useIsDarkTheme/index.ts
@@ -3,10 +3,12 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import type { DeepReadonly, Ref } from 'vue'
-import { ref, readonly, watch } from 'vue'
+import type { ComputedRef, DeepReadonly, Ref } from 'vue'
+
 import { createSharedComposable, usePreferredDark, useMutationObserver } from '@vueuse/core'
+import { ref, readonly, watch, inject, computed } from 'vue'
 import { checkIfDarkTheme } from '../../functions/isDarkTheme/index.ts'
+
 /**
  * Symbol used to provide the enforced dark / light theme state.
  * Injected by `NcThemeProvider`.
@@ -38,8 +40,27 @@ export function useIsDarkThemeElement(el: HTMLElement = document.body): DeepRead
 }
 
 /**
- * Shared composable to check whether the dark theme is enabled on the page.
- * Reacts on body data-theme-* attributes change and system theme change.
- * @return {DeepReadonly<Ref<boolean>>} - computed boolean whether the dark theme is enabled
+ * The real shared composable of the dark theme state.
+ * We need to wrap this to allow to react to injected theme changes.
  */
-export const useIsDarkTheme = createSharedComposable(() => useIsDarkThemeElement())
+const useInternalIsDarkTheme = createSharedComposable(() => useIsDarkThemeElement())
+
+/**
+ * Shared composable to check whether the dark theme is enabled on the page.
+ *
+ * Reacts on body data-theme-* attributes change and system theme change.
+ * As well as any enforced theme by the `NcThemeProvider`.
+ *
+ * @return Computed boolean whether the dark theme is enabled
+ */
+export function useIsDarkTheme(): DeepReadonly<Ref<boolean>> {
+	const isDarkTheme = useInternalIsDarkTheme()
+	const enforcedTheme = inject<ComputedRef<string>>(KEY_ENFORCE_THEME)
+
+	return computed(() => {
+		if (enforcedTheme?.value) {
+			return enforcedTheme.value === 'dark'
+		}
+		return isDarkTheme.value
+	})
+}
