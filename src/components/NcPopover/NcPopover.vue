603590d8cd56cdc986e4018144bab69840d7928b
Ferdinand Thiessen <opensource@fthiessen.de>
2025-04-18 23:37:01 +0200

    feat(NcPopover): add new `noFocusTrap` prop
    Replacing the `focusTrap` prop to have prop with default of `false`
    allowing to use HTML short cut attributes `<NcPopover no-focus-trap />`.
    
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- src/components/NcPopover/NcPopover.vue
+++ src/components/NcPopover/NcPopover.vue
@@ -208,13 +208,25 @@ export default {
 			type: String,
 			default: '',
 		},
+
 		/**
 		 * Enable popover focus trap
+		 *
+		 * @deprecated use noFocusTrap instead
 		 */
 		focusTrap: {
 			type: Boolean,
 			default: true,
 		},
+
+		/**
+		 * Disable the popover focus trap.
+		 */
+		noFocusTrap: {
+			type: Boolean,
+			default: false,
+		},
+
 		/**
 		 * Set element to return focus to after focus trap deactivation
 		 *
@@ -310,7 +322,7 @@ export default {
 		async useFocusTrap() {
 			await this.$nextTick()
 
-			if (!this.focusTrap) {
+			if (this.noFocusTrap || !this.focusTrap) {
 				return
 			}
 
