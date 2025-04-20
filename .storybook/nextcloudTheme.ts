b0d223efb5e60404c74f05f5c90b6735c43b3c69
Ferdinand Thiessen <opensource@fthiessen.de>
2025-04-20 18:57:00 +0200

    docs: theme storybook for Nextcloud
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

---
+++ .storybook/nextcloudTheme.ts
@@ -0,0 +1,12 @@
+import { create } from '@storybook/theming/create';
+// import logo from './logo.png?url'
+
+export default create({
+	base: 'light',
+
+	brandTitle: 'Nextcloud Vue components',
+	brandUrl: 'https://github.com/nextcloud-libraries/nextcloud-vue',
+	// brandImage: logo,
+	brandTarget: '_self',
+	colorPrimary: '#00679e',
+});
