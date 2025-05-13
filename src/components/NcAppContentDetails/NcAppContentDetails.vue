4e2199cc8c2ecefa469e9aa5471bf05cf3d674cd
Ferdinand Thiessen <opensource@fthiessen.de>
2025-05-13 12:24:17 +0200

    refactor(NcAppContentDetails): migrate component to Typescript
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- src/components/NcAppContentDetails/NcAppContentDetails.vue
+++ src/components/NcAppContentDetails/NcAppContentDetails.vue
@@ -3,15 +3,20 @@
   - SPDX-License-Identifier: AGPL-3.0-or-later
 -->
 
+<script setup lang="ts">
+import type { Slot } from 'vue'
+
+defineSlots<{
+	/**
+	 * Provide content to the details view.
+	 * Passing content to this slot is required!
+	 */
+	default?: Slot
+}>()
+</script>
+
 <template>
 	<div class="app-content-details">
-		<!-- @slot Provide content to the details view -->
 		<slot />
 	</div>
 </template>
-
-<script>
-export default {
-	name: 'NcAppContentDetails',
-}
-</script>
