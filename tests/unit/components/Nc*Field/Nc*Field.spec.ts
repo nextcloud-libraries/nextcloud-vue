02b20847e4f38ac55a57a706b3bed876b3483425
Ferdinand Thiessen <opensource@fthiessen.de>
2025-05-25 17:55:33 +0200

    refactor(NcInputField): migrate component to Typescript and script-setup
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

---
+++ tests/unit/components/Nc*Field/Nc*Field.spec.ts
@@ -0,0 +1,125 @@
+/**
+ * SPDX-FileCopyrightText: 2023 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import { mount } from '@vue/test-utils'
+import { describe, expect, it } from 'vitest'
+import NcInputField from '../../../../src/components/NcInputField/index.ts'
+import NcPasswordField from '../../../../src/components/NcPasswordField/index.ts'
+import NcTextField from '../../../../src/components/NcTextField/index.ts'
+
+// shared behavior between all components
+describe.each`
+name                 | component
+${'NcInputField'}    | ${NcInputField}
+${'NcPasswordField'} | ${NcPasswordField}
+${'NcTextField'}     | ${NcTextField}
+`('Nc*Field - shared tests for $name', ({ component }) => {
+	it('should emit changed text', async () => {
+		const wrapper = mount(component, {
+			props: {
+				label: 'label',
+				modelValue: '',
+				type: 'text',
+			},
+		})
+
+		await wrapper.get('input').setValue('text')
+
+		expect(wrapper.emitted('update:modelValue')).toEqual([['text']])
+	})
+
+	it('should have the placeholder set', async () => {
+		const wrapper = mount(component, {
+			props: {
+				modelValue: '',
+				label: 'The label',
+				placeholder: 'The placeholder',
+			},
+		})
+
+		expect(wrapper.find('input').attributes('placeholder')).toBe('The placeholder')
+
+		await wrapper.setProps({
+			label: 'The label',
+			placeholder: '',
+		})
+		expect(wrapper.find('input').attributes('placeholder')).toBe('The label')
+	})
+
+	it('should have the label set', () => {
+		const wrapper = mount(component, {
+			props: {
+				modelValue: '',
+				label: 'The label',
+			},
+		})
+
+		expect(wrapper.find('label').text()).toBe('The label')
+	})
+
+	it('should have the id set', () => {
+		const wrapper = mount(component, {
+			props: {
+				labelOutside: true,
+				modelValue: '',
+			},
+			attrs: {
+				id: 'some-id',
+			},
+		})
+
+		expect(wrapper.find('input').attributes('id')).toBe('some-id')
+	})
+
+	it('should have the helper text set', () => {
+		const wrapper = mount(component, {
+			props: {
+				helperText: 'Some helper text',
+				modelValue: '',
+			},
+		})
+
+		const id = wrapper.find('input').attributes('aria-describedby')
+		expect(wrapper.find(`#${id}`).exists()).toBe(true)
+		expect(wrapper.find(`#${id}`).text()).toBe('Some helper text')
+	})
+
+	it('should have the class set', () => {
+		const wrapper = mount(component, {
+			props: {
+				class: 'some-class',
+				modelValue: '',
+			},
+		})
+
+		expect(wrapper.classes('some-class')).toBe(true)
+	})
+
+	it('should have the input class set', () => {
+		const wrapper = mount(component, {
+			props: {
+				inputClass: 'some-class',
+				modelValue: '',
+			},
+		})
+
+		expect(wrapper.find('input').classes('some-class')).toBe(true)
+	})
+
+	it('should have the disabled attribute', async() => {
+		const wrapper = mount(component, {
+			props: {
+				modelValue: '',
+			},
+		})
+
+		expect(wrapper.find('input').attributes('disabled')).toBe(undefined)
+
+		await wrapper.setProps({
+			disabled: true
+		})
+		expect(wrapper.find('input').attributes('disabled')).toBe('')
+	})
+})
