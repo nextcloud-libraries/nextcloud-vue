6992bf212d78e4f379758b33e1f39fb2542338c7
Ferdinand Thiessen <opensource@fthiessen.de>
2025-06-23 15:47:34 +0200

    refactor(NcDateTimePickerNative): migrate component to Typescript and script-setup
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- src/components/NcActionInput/NcActionInput.vue
+++ src/components/NcActionInput/NcActionInput.vue
@@ -242,7 +242,7 @@ For the `NcSelect` component, all events will be passed through. Please see the 
 <script>
 import NcColorPicker from '../NcColorPicker/index.js'
 import NcDateTimePicker from '../NcDateTimePicker/index.js'
-import NcDateTimePickerNative from '../NcDateTimePickerNative/index.js'
+import NcDateTimePickerNative from '../NcDateTimePickerNative/index.ts'
 import NcPasswordField from '../NcPasswordField/index.ts'
 import NcSelect from '../NcSelect/index.js'
 import NcTextField from '../NcTextField/index.ts'
