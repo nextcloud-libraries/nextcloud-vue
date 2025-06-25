b01b23b7ec500ee883fe05d740c82f3f604d75cb
Ferdinand Thiessen <opensource@fthiessen.de>
2025-06-25 23:56:40 +0200

    feat(NcFilePicker): add picker component to select local files
    This can be used e.g. to upload files, for example in the forms app but
    also for the files or photos app.
    
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

---
+++ tests/component/components/NcFilePicker/NcFilePickerEmit.story.vue
@@ -0,0 +1,23 @@
+<!--
+  - SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+  - SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
+<script setup lang="ts">
+import NcFilePicker from '../../../../src/components/NcFilePicker/NcFilePicker.vue'
+
+defineProps({ ...NcFilePicker.props })
+
+const emit = defineEmits<{
+	pick: [fileNames: string[]]
+}>()
+
+function onPick(files: File[]) {
+	const fileNames = files.map((file) => file.webkitRelativePath || file.name)
+	emit('pick', fileNames)
+}
+</script>
+
+<template>
+	<NcFilePicker v-bind="$props" @pick="onPick" />
+</template>
