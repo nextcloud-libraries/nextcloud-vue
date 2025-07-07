0bbc7611ae50800a1ba1830df5c74034ce70ffb6
Ferdinand Thiessen <opensource@fthiessen.de>
2025-07-07 16:47:59 +0200

    fix(l10n): adjust for `@nextcloud/l10n` v3.4.0
    This fixes some components not beeing translated correctly.
    
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

---
+++ cypress/component/NcRichContenteditable.cy.ts
@@ -0,0 +1,36 @@
+/**
+ * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import NcRichContenteditable from '../../src/components/NcRichContenteditable/NcRichContenteditable.vue'
+
+it('Placeholder is set', () => {
+	cy.mount(NcRichContenteditable, {
+		propsData: {
+			modelValue: '',
+		},
+	})
+
+	cy.get('[role="textbox"]')
+		.should('be.visible')
+		.and('have.attr', 'aria-placeholder', 'Write a message …')
+})
+
+// does not work as Cypress is using webpack which does not have the l10n plugin
+it.skip('Placeholder is correctly translated', () => {
+	cy.window()
+		.then(($window) => {
+			$window.document.documentElement.lang = 'de'
+		})
+
+	cy.mount(NcRichContenteditable, {
+		propsData: {
+			modelValue: '',
+		},
+	})
+
+	cy.get('[role="textbox"]')
+		.should('be.visible')
+		.and('have.attr', 'aria-placeholder', 'Nachricht schreiben …')
+})
