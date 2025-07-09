6a842c7fe838063840a2cf09f69f6864a3989151
Ferdinand Thiessen <opensource@fthiessen.de>
2025-07-09 20:51:13 +0200

    feat(NcPasswordField): allow to set visibility state
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

---
+++ cypress/visual/NcPasswordField/NcPasswordField.story.vue
@@ -0,0 +1,36 @@
+<!--
+  - SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+  - SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
+<template>
+	<div class="story-wrapper">
+		<NcPasswordField :as-text="asText"
+			label="Password"
+			model-value="password"
+			:visible="visible" />
+	</div>
+</template>
+
+<script setup>
+import NcPasswordField from '../../../src/components/NcPasswordField/NcPasswordField.vue'
+
+defineProps({
+	asText: {
+		type: Boolean,
+		default: undefined,
+	},
+	visible: {
+		type: Boolean,
+		default: undefined,
+	},
+})
+</script>
+
+<style scoped>
+.story-wrapper {
+	margin-inline: auto;
+	max-width: 300px;
+	padding-top: 8px;
+}
+</style>
