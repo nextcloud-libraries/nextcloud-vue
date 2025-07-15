956072119a35894c060528b6ae41214e8546911c
Ferdinand Thiessen <opensource@fthiessen.de>
2025-07-15 18:31:49 +0200

    docs: clarify application containers and especially NcGuestContent
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- src/components/NcGuestContent/NcGuestContent.vue
+++ src/components/NcGuestContent/NcGuestContent.vue
@@ -8,13 +8,15 @@
 Guest content container to be used for the guest content of your app.
 
 This components provides a wrapper around guest page content.
-It should be used as the main wrapper for public pages, similar to `NcContent`.
+It should be used as the main wrapper for guest pages, similar to `NcContent` for public and user pages.
+Meaning this component must be used as the root component for your application
+if you render the page using `OCP\AppFramework\Http\TemplateResponse::RENDER_AS_GUEST`.
 
 It can't be used multiple times on the same page.
 
 ### Usage
 
-```vue
+```vue static
 <template>
 	<NcGuestContent>
 		<h2>Hello guest</h2>
@@ -26,6 +28,7 @@ It can't be used multiple times on the same page.
 
 <script setup lang="ts">
 import type { Slot } from 'vue'
+
 import { onMounted, onUnmounted } from 'vue'
 
 defineSlots<{
