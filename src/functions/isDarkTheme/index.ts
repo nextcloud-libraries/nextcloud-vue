97304733eaa52cc003af91a623f23b88d83210d6
John Molakvoæ <skjnldsv@users.noreply.github.com>
2025-07-29 12:40:21 +0200

    chore: cleaner code syntax
    Co-authored-by: Grigorii K. Shartsev <me@shgk.me>
    Signed-off-by: John Molakvoæ <skjnldsv@users.noreply.github.com>

--- src/functions/isDarkTheme/index.ts
+++ src/functions/isDarkTheme/index.ts
@@ -20,10 +20,19 @@ export function checkIfDarkTheme(el: HTMLElement = document.body): boolean {
 	// 2. second priority is the data-themes attribute on the body, aka the user setting
 	// 3. third priority is the system preference
 	// 4. lastly is the default light theme
-	const darkModeSystemPreference = window?.matchMedia?.('(prefers-color-scheme: dark)')?.matches
-	const darkModeAccountSetting = document.body.getAttribute('data-themes')?.includes('dark')
-	const darkModeElementOverride = el.closest('[data-theme-dark]') !== null
-	return darkModeElementOverride || darkModeAccountSetting || darkModeSystemPreference || false
+	if (el.closest('[data-theme-dark]') !== null) {
+		return true
+	}
+
+	if (document.body.getAttribute('data-themes')?.includes('dark')) {
+		return true
+	}
+
+	if (window.matchMedia('(prefers-color-scheme: dark)')?.matches) {
+		return true
+	}
+
+	return false
 }
 
 /**
