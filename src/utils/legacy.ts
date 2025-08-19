7dcb93b6eb222e6827e0048378bf2717e412816b
skjnldsv <skjnldsv@protonmail.com>
2025-08-19 14:51:58 +0200

    fix(utils): legacy check syntax
    Signed-off-by: skjnldsv <skjnldsv@protonmail.com>

--- src/utils/legacy.ts
+++ src/utils/legacy.ts
@@ -1,6 +1,7 @@
-/*
+/**
  * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-export const isLegacy32 = Number.parseInt(window.OC?.config.version.split('.')[0] ?? '0') < 32
+const version = window.OC?.config?.version?.split('.')[0] || '32'
+export const isLegacy32 = Number.parseInt(version) < 32
