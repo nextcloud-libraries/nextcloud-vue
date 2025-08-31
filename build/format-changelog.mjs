03dbef3ff6c70519769c5fc73a8c80271a75bf6b
Grigorii K. Shartsev <me@shgk.me>
2025-08-31 13:56:09 +0200

    chore(lint): fix `no-useless-escape`
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- build/format-changelog.mjs
+++ build/format-changelog.mjs
@@ -12,7 +12,7 @@ const content = await readFile(file, { encoding: 'utf-8' })
 
 const formatted = content.replaceAll(
 	/by @([^ ]+) in ((https:\/\/github.com\/)nextcloud-libraries\/nextcloud-vue\/pull\/(\d+))/g,
-	'[\#$4]($2) \([$1]($3$1)\)',
+	'[\\#$4]($2) \\([$1]($3$1)\\)',
 )
 
 if (formatted !== content) {
