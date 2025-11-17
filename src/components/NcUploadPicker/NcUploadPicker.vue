8833c9059c690942af540db4c3aae962333c4bc0
Ferdinand Thiessen <opensource@fthiessen.de>
2025-11-17 17:20:15 +0100

    refactor(NcUploadPicker): migrate component to script-setup
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- src/components/NcUploadPicker/NcUploadPicker.vue
+++ src/components/NcUploadPicker/NcUploadPicker.vue
@@ -168,15 +168,16 @@
 	</form>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import type { NewMenuEntry, Node } from '@nextcloud/files'
 import type { Upload } from '@nextcloud/upload'
 import type { PropType } from 'vue'
 
-import { uploadConflictHandler } from '@nextcloud/dialogs'
-import { Folder, getNewFileMenuEntries, NewMenuEntryCategory } from '@nextcloud/files'
-import { getUploader, UploaderStatus, UploadStatus } from '@nextcloud/upload'
-import { defineComponent } from 'vue'
+import { openConflictPicker } from '@nextcloud/dialogs'
+import { Folder, getNewFileMenuEntries, getUniqueName, NewMenuEntryCategory } from '@nextcloud/files'
+import { getConflicts, getUploader, UploaderStatus, UploadStatus } from '@nextcloud/upload'
+import { basename } from 'node:path'
+import { computed, getCurrentInstance, nextTick, onBeforeMount, ref, watch } from 'vue'
 import IconCancel from 'vue-material-design-icons/Cancel.vue'
 import IconFolderUpload from 'vue-material-design-icons/FolderUpload.vue'
 import IconPlus from 'vue-material-design-icons/Plus.vue'
@@ -191,323 +192,266 @@ import NcProgressBar from '../NcProgressBar/NcProgressBar.vue'
 import { t } from '../../l10n.ts'
 import { logger } from '../../utils/logger.ts'
 
-export default defineComponent({
-	name: 'UploadPicker',
-
-	components: {
-		IconCancel,
-		IconFolderUpload,
-		IconPlus,
-		IconUpload,
-		NcActionButton,
-		NcActionCaption,
-		NcActionSeparator,
-		NcActions,
-		NcButton,
-		NcIconSvgWrapper,
-		NcProgressBar,
+const props = defineProps({
+	accept: {
+		type: Array as PropType<string[]>,
+		default: null,
 	},
 
-	props: {
-		accept: {
-			type: Array as PropType<string[]>,
-			default: null,
-		},
-
-		disabled: {
-			type: Boolean,
-			default: false,
-		},
-
-		multiple: {
-			type: Boolean,
-			default: false,
-		},
-
-		/**
-		 * Allow to disable the "new"-menu for this UploadPicker instance
-		 */
-		noMenu: {
-			type: Boolean,
-			default: false,
-		},
-
-		/**
-		 * Allow to disable the button label
-		 */
-		noLabel: {
-			type: Boolean,
-			default: false,
-		},
-
-		/**
-		 * Make the "New"-button primary color.
-		 */
-		primary: {
-			type: Boolean,
-			default: false,
-		},
-
-		destination: {
-			type: Folder,
-			default: undefined,
-		},
-
-		allowFolders: {
-			type: Boolean,
-			default: false,
-		},
-
-		/**
-		 * List of file present in the destination folder
-		 * It is also possible to provide a function that takes a relative path to the current directory and returns the content of it
-		 * Note: If a function is passed it should return the current base directory when no path or an empty is passed
-		 */
-		content: {
-			type: [Array, Function] as PropType<Node[] | ((relativePath?: string) => Node[] | PromiseLike<Node[]>)>,
-			default: () => [],
-		},
-
-		/**
-		 * Overwrite forbidden characters (by default the capabilities of the server are used)
-		 *
-		 * @deprecated Deprecated and will be removed in the next major version
-		 */
-		forbiddenCharacters: {
-			type: Array as PropType<string[]>,
-			default: () => [],
-		},
+	disabled: {
+		type: Boolean,
+		default: false,
 	},
 
-	setup() {
-		return {
-			t,
-
-			// non reactive data / properties
-			progressTimeId: `nc-uploader-progress-${Math.random().toString(36).slice(7)}`,
-		}
+	multiple: {
+		type: Boolean,
+		default: false,
 	},
 
-	data() {
-		return {
-			newFileMenuEntries: [] as NewMenuEntry[],
-			openedMenu: false,
-			uploadManager: getUploader(),
-		}
+	/**
+	 * Allow to disable the "new"-menu for this UploadPicker instance
+	 */
+	noMenu: {
+		type: Boolean,
+		default: false,
 	},
 
-	computed: {
-		menuEntriesUpload(): NewMenuEntry[] {
-			return this.newFileMenuEntries.filter((entry) => entry.category === NewMenuEntryCategory.UploadFromDevice)
-		},
-
-		menuEntriesNew(): NewMenuEntry[] {
-			return this.newFileMenuEntries.filter((entry) => entry.category === NewMenuEntryCategory.CreateNew)
-		},
-
-		menuEntriesOther(): NewMenuEntry[] {
-			return this.newFileMenuEntries.filter((entry) => entry.category === NewMenuEntryCategory.Other)
-		},
-
-		/**
-		 * Check whether the current browser supports uploading directories
-		 * Hint: This does not check if the current connection supports this, as some browsers require a secure context!
-		 */
-		canUploadFolders(): boolean {
-			return this.allowFolders && 'webkitdirectory' in document.createElement('input')
-		},
-
-		queue(): Upload[] {
-			return this.uploadManager.queue as Upload[]
-		},
-
-		hasFailure(): boolean {
-			return this.queue.some((upload: Upload) => upload.status === UploadStatus.FAILED)
-		},
-
-		isAssembling(): boolean {
-			return this.queue.some((upload: Upload) => upload.status === UploadStatus.ASSEMBLING)
-		},
-
-		isUploading(): boolean {
-			return this.queue.some((upload: Upload) => upload.status !== UploadStatus.CANCELLED)
-		},
-
-		isOnlyAssembling(): boolean {
-			return this.isAssembling
-				&& this.queue.every((upload: Upload) => (
-					// ignore empty uploads or meta uploads
-					upload.size === 0
-					// all the uploads are assembling or finished
-					|| upload.status === UploadStatus.ASSEMBLING
-					|| upload.status === UploadStatus.FINISHED
-				))
-		},
-
-		isPaused(): boolean {
-			return this.uploadManager.info?.status === UploaderStatus.PAUSED
-		},
-
-		buttonLabel(): string {
-			return this.noMenu ? t('Upload') : t('New')
-		},
-
-		haveMenu(): boolean {
-			return !((this.noMenu || this.newFileMenuEntries.length === 0) && !this.canUploadFolders)
-		},
+	/**
+	 * Allow to disable the button label
+	 */
+	noLabel: {
+		type: Boolean,
+		default: false,
 	},
 
-	watch: {
-		allowFolders: {
-			immediate: true,
-			handler() {
-				if (typeof this.content !== 'function' && this.allowFolders) {
-					logger.error('[UploadPicker] Setting `allowFolders` is only allowed if `content` is a function')
-				}
-			},
-		},
-
-		destination(destination) {
-			this.setDestination(destination)
-		},
-
-		isPaused(isPaused) {
-			if (isPaused) {
-				this.$emit('paused', this.queue)
-			} else {
-				this.$emit('resumed', this.queue)
-			}
-		},
+	/**
+	 * Make the "New"-button primary color.
+	 */
+	primary: {
+		type: Boolean,
+		default: false,
 	},
 
-	beforeMount() {
-		// Prevent init with wrong destination
-		if (this.destination) {
-			this.setDestination(this.destination)
-		}
-
-		// Update data on upload progress
-		this.uploadManager.addNotifier(this.onUploadCompletion)
-
-		logger.debug('UploadPicker initialised')
+	destination: {
+		type: Folder,
+		default: undefined,
 	},
 
-	methods: {
-		etaTimeAndSpeed(): string {
-			const speed = this.uploadManager.eta.speedReadable
-			if (speed) {
-				return `${this.uploadManager.eta.timeReadable} (${speed})`
-			}
-			return this.uploadManager.eta.timeReadable
-		},
+	allowFolders: {
+		type: Boolean,
+		default: false,
+	},
 
-		/**
-		 * Handle clicking a new-menu entry
-		 *
-		 * @param entry The entry that was clicked
-		 */
-		async onClick(entry: NewMenuEntry) {
-			entry.handler(
-				this.destination!,
-				await this.getContent().catch(() => []),
-			)
-		},
+	/**
+	 * List of file present in the destination folder
+	 * It is also possible to provide a function that takes a relative path to the current directory and returns the content of it
+	 * Note: If a function is passed it should return the current base directory when no path or an empty is passed
+	 */
+	content: {
+		type: [Array, Function] as PropType<Node[] | ((relativePath?: string) => Node[] | PromiseLike<Node[]>)>,
+		default: () => [],
+	},
 
-		/**
-		 * Trigger file picker
-		 *
-		 * @param uploadFolders Upload folders
-		 */
-		onTriggerPick(uploadFolders: boolean = false) {
-			const input = this.$refs.input as HTMLInputElement
-			// Setup directory picking if enabled
-			if (this.canUploadFolders) {
-				input.webkitdirectory = uploadFolders
-			}
-			// Trigger click on the input to open the file picker
-			this.$nextTick(() => input.click())
-		},
-
-		/**
-		 * Helper for backwards compatibility that queries the content of the current directory
-		 *
-		 * @param path The current path
-		 */
-		async getContent(path?: string) {
-			return Array.isArray(this.content) ? this.content : await this.content(path)
-		},
-
-		/**
-		 * Start uploading
-		 */
-		async onPick() {
-			const input = this.$refs.input as HTMLInputElement
-			const files = input.files ? Array.from(input.files) : []
-
-			try {
-				await this.uploadManager
-					.batchUpload('', files, uploadConflictHandler(this.getContent))
-			} catch (error) {
-				logger.debug('Error while uploading', { error })
-			} finally {
-				this.resetForm()
-			}
-		},
-
-		resetForm() {
-			const form = this.$refs.form as HTMLFormElement
-			form?.reset()
-		},
-
-		/**
-		 * Cancel ongoing queue
-		 */
-		onCancel() {
-			this.uploadManager.queue.forEach((upload: Upload) => {
-				upload.cancel()
-			})
-			this.resetForm()
-		},
-
-		setDestination(destination: Folder) {
-			if (!this.destination) {
-				logger.debug('Invalid destination')
-				return
-			}
-
-			this.uploadManager.destination = destination
-
-			// If the destination change, we need to refresh the menu
-			this.newFileMenuEntries = getNewFileMenuEntries(destination)
-		},
-
-		onUploadCompletion(upload: Upload) {
-			if (upload.status === UploadStatus.FAILED) {
-				this.$emit('failed', upload)
-			} else {
-				this.$emit('uploaded', upload)
-			}
-		},
-
-		onKeyDown(event: KeyboardEvent) {
-			// Shift + u opens the menu
-			if (event.key === 'u') {
-				// If we have a menu, open it
-				if (this.haveMenu) {
-					this.openedMenu = true
-					return
-				}
-
-				// Otherwise, trigger the default action
-				this.onTriggerPick()
-			}
-
-			if (event.key === 'Escape' && this.openedMenu) {
-				this.openedMenu = false
-			}
-		},
+	/**
+	 * Overwrite forbidden characters (by default the capabilities of the server are used)
+	 *
+	 * @deprecated Deprecated and will be removed in the next major version
+	 */
+	forbiddenCharacters: {
+		type: Array as PropType<string[]>,
+		default: () => [],
 	},
 })
+
+const emit = defineEmits<{
+	uploaded: [upload: Upload]
+	failed: [upload: Upload]
+	paused: [queue: Upload[]]
+	resumed: [queue: Upload[]]
+}>()
+
+const uploadManager = getUploader()
+onBeforeMount(() => {
+	uploadManager.addNotifier(onUploadCompletion)
+})
+watch(() => props.destination, () => {
+	setDestination(props.destination!)
+}, { immediate: true })
+
+const progressTimeId = `nc-uploader-progress-${Math.random().toString(36).slice(7)}`
+
+const newFileMenuEntries = ref<NewMenuEntry[]>([])
+const openedMenu = ref(false)
+
+const menuEntriesUpload = computed(() => newFileMenuEntries.value.filter((entry) => entry.category === NewMenuEntryCategory.UploadFromDevice))
+const menuEntriesNew = computed(() => newFileMenuEntries.value.filter((entry) => entry.category === NewMenuEntryCategory.CreateNew))
+const menuEntriesOther = computed(() => newFileMenuEntries.value.filter((entry) => entry.category === NewMenuEntryCategory.Other))
+
+/**
+ * Check whether the current browser supports uploading directories
+ * Hint: This does not check if the current connection supports this, as some browsers require a secure context!
+ */
+const canUploadFolders = computed(() => props.allowFolders && 'webkitdirectory' in document.createElement('input'))
+const queue = computed(() => uploadManager.queue as Upload[])
+const hasFailure = computed(() => queue.value.some((upload: Upload) => upload.status === UploadStatus.FAILED))
+const isUploading = computed(() => queue.value.some((upload: Upload) => upload.status !== UploadStatus.CANCELLED))
+const isAssembling = computed(() => queue.value.some((upload: Upload) => upload.status === UploadStatus.ASSEMBLING))
+const isOnlyAssembling = computed(() => {
+	return isAssembling.value
+		&& queue.value.every((upload: Upload) => (
+			// ignore empty uploads or meta uploads
+			upload.size === 0
+			// all the uploads are assembling or finished
+			|| upload.status === UploadStatus.ASSEMBLING
+			|| upload.status === UploadStatus.FINISHED
+		))
+})
+const buttonLabel = computed(() => {
+	return props.noMenu ? t('Upload') : t('New')
+})
+const haveMenu = computed(() => {
+	return !((props.noMenu || newFileMenuEntries.value.length === 0) && !canUploadFolders.value)
+})
+
+const isPaused = computed(() => uploadManager.info?.status === UploaderStatus.PAUSED)
+watch(isPaused, (isPaused) => {
+	if (isPaused) {
+		emit('paused', queue.value)
+	} else {
+		emit('resumed', queue.value)
+	}
+})
+
+/**
+ *
+ */
+function etaTimeAndSpeed(): string {
+	const speed = uploadManager.eta.speedReadable
+	if (speed) {
+		return `${uploadManager.eta.timeReadable} (${speed})`
+	}
+	return uploadManager.eta.timeReadable
+}
+
+/**
+ * Handle clicking a new-menu entry
+ *
+ * @param entry The entry that was clicked
+ */
+async function onClick(entry: NewMenuEntry) {
+	entry.handler(
+		props.destination!,
+		await getContent().catch(() => []),
+	)
+}
+
+/**
+ * Trigger file picker
+ *
+ * @param uploadFolders Upload folders
+ */
+function onTriggerPick(uploadFolders: boolean = false) {
+	const input = getCurrentInstance()!.refs.input as HTMLInputElement
+	// Setup directory picking if enabled
+	if (canUploadFolders.value) {
+		input.webkitdirectory = uploadFolders
+	}
+	// Trigger click on the input to open the file picker
+	nextTick(() => input.click())
+}
+
+/**
+ * Helper for backwards compatibility that queries the content of the current directory
+ *
+ * @param path The current path
+ */
+async function getContent(path?: string) {
+	return Array.isArray(props.content) ? props.content : await props.content(path)
+}
+
+/**
+ * Start uploading
+ */
+async function onPick() {
+	const input = getCurrentInstance()!.refs.input as HTMLInputElement
+	const files = input.files ? Array.from(input.files) : []
+
+	try {
+		await uploadManager
+			.batchUpload('', files, async (nodes, currentPath) => {
+				const content = await getContent(currentPath)
+				const conflicts = getConflicts(nodes, content)
+				if (conflicts.length === 0) {
+					return nodes
+				}
+
+				const result = await openConflictPicker(basename(currentPath), conflicts, content, { recursive: props.allowFolders })
+				if (result) {
+					const names: string[] = content.map((n) => n.displayname)
+					for (const node of result.renamed) {
+						const newName = getUniqueName(node.name, names)
+						names.push(newName)
+						Object.defineProperty(node, 'name', newName)
+					}
+					return [
+						...result.selected,
+						...result.renamed,
+					]
+				}
+				return false
+			})
+	} catch (error) {
+		logger.debug('Error while uploading', { error })
+	} finally {
+		resetForm()
+	}
+}
+
+/**
+ *
+ */
+function resetForm() {
+	const form = getCurrentInstance()!.refs.form as HTMLFormElement
+	form?.reset()
+}
+
+/**
+ * Cancel ongoing queue
+ */
+function onCancel() {
+	uploadManager.queue.forEach((upload: Upload) => {
+		upload.cancel()
+	})
+	resetForm()
+}
+
+/**
+ *
+ * @param destination
+ */
+function setDestination(destination: Folder) {
+	if (!props.destination) {
+		logger.debug('Invalid destination')
+		return
+	}
+
+	uploadManager.destination = destination
+
+	// If the destination change, we need to refresh the menu
+	newFileMenuEntries.value = getNewFileMenuEntries(destination)
+}
+
+/**
+ *
+ * @param upload
+ */
+function onUploadCompletion(upload: Upload) {
+	if (upload.status === UploadStatus.FAILED) {
+		emit('failed', upload)
+	} else {
+		emit('uploaded', upload)
+	}
+}
 </script>
 
 <style lang="scss" scoped>
