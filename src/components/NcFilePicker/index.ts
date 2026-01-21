06b7afea8fe39ba01085baf0cd89b2827a571b80
Ferdinand Thiessen <opensource@fthiessen.de>
2026-01-21 13:08:08 +0100

    refactor: use `actions` prop instead of slot
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- src/components/NcFilePicker/index.ts
+++ src/components/NcFilePicker/index.ts
@@ -1,6 +1,11 @@
-/**
+/*!
  * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
+
+export type {
+	FilePickerItem,
+	FilePickerItemGroup,
+} from './NcFilePicker.vue'
 
 export { default } from './NcFilePicker.vue'
