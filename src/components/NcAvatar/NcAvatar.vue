c05664111fc32575911f0aed981dc7e733c86463
kristian-zendato <kristian.zendato@nextcloud.com>
2026-09-02 12:01:35 +0000

    fix: profile hovercard for disabled user
    fix: profile hovercard for disabled user
    
    Signed-off-by: kristian-zendato <kristian.zendato@nextcloud.com>
    
    [skip ci]

--- src/components/NcAvatar/NcAvatar.vue
+++ src/components/NcAvatar/NcAvatar.vue
@@ -272,6 +272,7 @@ export default {
 			</template>
 			<NcProfileHoverCard
 				:user="user"
+				:displayName="hoverCardDisplayName"
 				:open="contactsMenuOpenState"
 				:actions="hoverCardActions"
 				:actions-loading="contactsMenuLoading"
@@ -718,6 +719,15 @@ export default {
 				return null
 			}
 			return this.menu.length > 0 ? this.menu : null
+		},
+
+		/**
+		 * Best available display name for the hover card (prop or contacts menu).
+		 *
+		 * @return {string|undefined}
+		 */
+		hoverCardDisplayName() {
+			return this.displayName || this.contactsMenuData?.fullName || undefined
 		},
 
 		/**
